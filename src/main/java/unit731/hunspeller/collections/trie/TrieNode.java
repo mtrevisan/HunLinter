99ac1e3635fc033d7032b0d6cc1cc4a7bcbb461e
Mauro Trevisan <mauro.simioni@targaubiest.com>
2017-09-21 10:12:34 +0200

    no message

--- src/main/java/unit731/hunspeller/collections/trie/TrieNode.java
+++ src/main/java/unit731/hunspeller/collections/trie/TrieNode.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import lombok.Getter;
-import lombok.Setter;
 
 
 public class TrieNode<T> implements Cloneable{
