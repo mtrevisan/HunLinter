9365f04cdd3bcb5d42a29405f5b8ca8a38cf8206
Mauro Trevisan <mauro.simioni@targaubiest.com>
2017-10-10 15:16:33 +0200

    no message

--- src/main/java/unit731/hunspeller/collections/trie/TrieNode.java
+++ src/main/java/unit731/hunspeller/collections/trie/TrieNode.java
@@ -1,12 +1,9 @@
 package unit731.hunspeller.collections.trie;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.function.Consumer;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
