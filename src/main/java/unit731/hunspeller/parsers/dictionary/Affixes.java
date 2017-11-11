f5f7395e0cf2d24aa43dfea21db57c3318ba1e7b
Mauro Trevisan <mauro.trevisan@gmail.com>
2017-11-11 21:56:04 +0100

    no message

---
+++ src/main/java/unit731/hunspeller/parsers/dictionary/Affixes.java
@@ -0,0 +1,16 @@
+package unit731.hunspeller.parsers.dictionary;
+
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@AllArgsConstructor
+@Getter
+public class Affixes{
+
+	private final Set<String> terminalAffixes;
+	private final Set<String> prefixes;
+	private final Set<String> suffixes;
+
+}
