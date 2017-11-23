77939dfed71ef18d9a2d5af8ce83cdda905ee246
Mauro Trevisan <mauro.simioni@targaubiest.com>
2017-11-23 17:55:31 +0100

    no message

---
+++ src/test/java/unit731/hunspeller/collections/bloomfilter/core/BitArrayTest.java
@@ -0,0 +1,52 @@
+package unit731.hunspeller.collections.bloomfilter.core;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class BitArrayTest{
+
+	private static final int MAX = 100 * 100;
+
+
+	@Test
+	public void fast(){
+		FastBitArray bits = new FastBitArray(MAX);
+		for(int i = 0; i < MAX; i ++){
+			Assert.assertFalse(bits.get(i));
+			bits.set(i);
+			Assert.assertTrue(bits.get(i));
+			bits.clear(i);
+			Assert.assertFalse(bits.get(i));
+		}
+	}
+
+	@Test
+	public void java(){
+		JavaBitArray bits = new JavaBitArray(MAX);
+		for(int i = 0; i < MAX; i ++){
+			Assert.assertFalse(bits.get(i));
+			bits.set(i);
+			Assert.assertTrue(bits.get(i));
+			bits.clear(i);
+			Assert.assertFalse(bits.get(i));
+		}
+	}
+
+	@Test
+	public void memoryMappedFile() throws IOException{
+		File file = File.createTempFile("hunspeller-duplications-bitarray", ".bits");
+		file.deleteOnExit();
+		MemoryMappedFileBitArray bits = new MemoryMappedFileBitArray(file, MAX);
+		for(int i = 0; i < MAX; i ++){
+			Assert.assertFalse(bits.get(i));
+			bits.set(i);
+			Assert.assertTrue(bits.get(i));
+			bits.clear(i);
+			Assert.assertFalse(bits.get(i));
+		}
+	}
+
+}
