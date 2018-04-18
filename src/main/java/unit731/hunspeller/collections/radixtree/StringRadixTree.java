28e10f84ee5ee76f8fc5b49c8a8a5b1a0f0dd47e
Mauro Trevisan <mauro.simioni@targaubiest.com>
2018-04-18 12:10:24 +0200

    no message

--- src/main/java/unit731/hunspeller/collections/radixtree/StringRadixTree.java
+++ src/main/java/unit731/hunspeller/collections/radixtree/StringRadixTree.java
@@ -2,11 +2,28 @@ package unit731.hunspeller.collections.radixtree;
 
 import unit731.hunspeller.collections.radixtree.tree.RadixTree;
 import java.io.Serializable;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import unit731.hunspeller.collections.radixtree.sequencers.SequencerInterface;
+import unit731.hunspeller.collections.radixtree.sequencers.StringSequencer;
+import unit731.hunspeller.collections.radixtree.tree.RadixTreeNode;
 
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class StringRadixTree<V extends Serializable> extends RadixTree<String, V>{
+
+	public static <T extends Serializable> StringRadixTree<T> createTree(){
+		return new StringRadixTree<>();
+	}
+
+	public static <T extends Serializable> StringRadixTree<T> createTreeNoDuplicates(){
+		StringRadixTree<T> tree = new StringRadixTree<>();
+		tree.noDuplicatesAllowed = true;
+		return tree;
+	}
+
+	private StringRadixTree(){
+		SequencerInterface<String> seq = new StringSequencer();
+
+		root = RadixTreeNode.createEmptyNode(seq.getNullSequence());
+		this.sequencer = seq;
+	}
 
 }
