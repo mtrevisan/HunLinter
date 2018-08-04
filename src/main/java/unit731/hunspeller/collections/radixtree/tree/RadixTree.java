c8b164abc228832d3861f56aaa976e3a3722032d
Mauro Trevisan <mauro.trevisan@gmail.com>
2018-08-04 20:41:35 +0200

    no message

--- src/main/java/unit731/hunspeller/collections/radixtree/tree/RadixTree.java
+++ src/main/java/unit731/hunspeller/collections/radixtree/tree/RadixTree.java
@@ -43,7 +43,7 @@ import unit731.hunspeller.collections.radixtree.sequencers.SequencerInterface;
  */
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class RadixTree<S, V extends Serializable> implements Map<S, V>, Serializable{
+public class RadixTree<S, V extends Serializable> implements Map<S, V>{
 
 	private static final long serialVersionUID = -5213027224293608217L;
 
