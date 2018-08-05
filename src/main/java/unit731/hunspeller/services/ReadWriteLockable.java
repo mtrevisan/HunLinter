f350728ebeed63369f27b808e0fb6c79d6281053
Mauro Trevisan <mauro.trevisan@gmail.com>
2018-08-05 23:09:45 +0200

    no message

--- src/main/java/unit731/hunspeller/services/ReadWriteLockable.java
+++ src/main/java/unit731/hunspeller/services/ReadWriteLockable.java
@@ -2,6 +2,9 @@ package unit731.hunspeller.services;
 
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.StampedLock;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 
 //https://dzone.com/articles/java-concurrency-read-write-lo
@@ -11,6 +14,8 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class ReadWriteLockable{
 
 	private static final ReadWriteLock READ_WRITE_LOCK = new ReentrantReadWriteLock();
+
+	private static final StampedLock RW_LOCK = new StampedLock();
 
 
 	public void acquireReadLock(){
@@ -28,5 +33,42 @@ public class ReadWriteLockable{
 	protected void releaseWriteLock(){
 		READ_WRITE_LOCK.writeLock().unlock();
 	}
-	
+
+	public <T> void w(Supplier<T> getter, Consumer<T> setter, T newState){
+		long stamp = RW_LOCK.readLock();
+		try{
+			T oldState = getter.get();
+			while(newState == oldState){
+				long writeStamp = RW_LOCK.tryConvertToWriteLock(stamp);
+				if(writeStamp != 0l){
+					stamp = writeStamp;
+
+					setter.accept(oldState);
+					break;
+				}
+
+				RW_LOCK.unlockRead(stamp);
+				stamp = RW_LOCK.writeLock();
+			}
+		}
+		finally{
+			RW_LOCK.unlock(stamp);
+		}
+	}
+
+	public <T> T r(Supplier<T> getter){
+		long stamp = RW_LOCK.tryOptimisticRead();
+		T currState = getter.get();
+		if(!RW_LOCK.validate(stamp)){
+			stamp = RW_LOCK.readLock();
+			try{
+				currState = getter.get();
+			}
+			finally{
+				RW_LOCK.unlockRead(stamp);
+			}
+		}
+		return currState;
+	}
+
 }
