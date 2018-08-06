bfcd9de1d1f224cd69f85f110d40dab3ab587825
Mauro Trevisan <mauro.simioni@targaubiest.com>
2018-08-06 16:12:30 +0200

    no message

--- src/test/java/unit731/hunspeller/parsers/dictionary/WordGeneratorTest.java
+++ src/test/java/unit731/hunspeller/parsers/dictionary/WordGeneratorTest.java
@@ -86,36 +86,36 @@ public class WordGeneratorTest{
 		Assert.assertEquals(new Production("p1aas1s2", "", strategy), stems.get(4));
 	}
 
-//	@Test
-//	public void stems2() throws IOException{
-//		StringJoiner sj = new StringJoiner("\n");
-//		String content = sj.add("SET UTF-8")
-//			.add("FLAG long")
-//			.add("SFX S1 Y 1")
-//			.add("SFX S1 0 s1/S2")
-//			.add("SFX S2 Y 1")
-//			.add("SFX S2 0 s2/P1")
-//			.add("PFX P1 Y 1")
-//			.add("PFX P1 0 p1")
-//			.toString();
-//		File affFile = FileService.getTemporaryUTF8File(content);
-//		affParser.parse(affFile);
-//		strategy = affParser.getFlagParsingStrategy();
-//
-//		String line = "aa/S1";
-//		List<Production> stems = wordGenerator.applyRules(line);
-//
-//		Assert.assertEquals(4, stems.size());
-//		//base production
-//		Assert.assertEquals(new Production("aa", "S1", strategy), stems.get(0));
-//		//onefold productions
-//		Assert.assertEquals(new Production("aas1", "S2", strategy), stems.get(1));
-//		//twofold productions
-//		Assert.assertEquals(new Production("aas1s2", "P1", strategy), stems.get(2));
-//		//lastfold productions
-//		Assert.assertEquals(new Production("p1aas1s2", "", strategy), stems.get(3));
-//	}
-//
+	@Test
+	public void stems2() throws IOException{
+		StringJoiner sj = new StringJoiner("\n");
+		String content = sj.add("SET UTF-8")
+			.add("FLAG long")
+			.add("SFX S1 Y 1")
+			.add("SFX S1 0 s1/S2")
+			.add("SFX S2 Y 1")
+			.add("SFX S2 0 s2/P1")
+			.add("PFX P1 Y 1")
+			.add("PFX P1 0 p1")
+			.toString();
+		File affFile = FileService.getTemporaryUTF8File(content);
+		affParser.parse(affFile);
+		strategy = affParser.getFlagParsingStrategy();
+
+		String line = "aa/S1";
+		List<Production> stems = wordGenerator.applyRules(line);
+
+		Assert.assertEquals(4, stems.size());
+		//base production
+		Assert.assertEquals(new Production("aa", "S1", strategy), stems.get(0));
+		//onefold productions
+		Assert.assertEquals(new Production("aas1", "S2", strategy), stems.get(1));
+		//twofold productions
+		//lastfold productions
+		Assert.assertEquals(new Production("aas1s2", "P1", strategy), stems.get(2));
+		Assert.assertEquals(new Production("p1aas1s2", "", strategy), stems.get(3));
+	}
+
 //	@Test
 //	public void stems3() throws IOException{
 //		StringJoiner sj = new StringJoiner("\n");
