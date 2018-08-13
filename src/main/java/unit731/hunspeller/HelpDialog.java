511c9f4b2b044ea890da8753392d5d96c89d9723
Mauro Trevisan <mauro.trevisan@gmail.com>
2018-08-13 09:35:07 +0200

    no message

--- src/main/java/unit731/hunspeller/HelpDialog.java
+++ src/main/java/unit731/hunspeller/HelpDialog.java
@@ -65,6 +65,16 @@ public class HelpDialog extends JDialog{
 		lblProductNameOut.setText(artifactID);
 		lblProductVersionOut.setText(version);
 		lblReleaseDateOut.setText(DictionaryParser.DATE_FORMATTER.format(buildTimestamp));
+		lblManagedOptionsTextArea.setText(
+			"General:\n"
+			+ "\tSET, FLAG, COMPLEXPREFIXES, LANG, AF, AM\n"
+			+ "Compounding:\n"
+			+ "\tCOMPOUNDRULE, COMPOUNDMIN, COMPOUNDFLAG, ONLYINCOMPOUND, CIRCUMFIX, CHECKCOMPOUNDTRIPLE, SIMPLIFIEDTRIPLE\n"
+			+ "Affix creation:\n"
+			+ "\tPFX, SFX\n"
+			+ "Others:\n"
+			+ "\tFULLSTRIP, KEEPCASE, NEEDAFFIX, ICONV, OCONV");
+		lblManagedOptionsTextArea.setCaretPosition(0);
 		lblCopyrightOut.setText("Copyright © " + DictionaryParser.YEAR_FORMATTER.format(LocalDate.now()) + " Mauro Trevisan");
 	}
 
@@ -78,6 +88,9 @@ public class HelpDialog extends JDialog{
       lblCopyrightOut = new javax.swing.JLabel();
       lblProductNameOut = new javax.swing.JLabel();
       lblLogo = new javax.swing.JLabel();
+      lblManagedOptionsLabel = new javax.swing.JLabel();
+      jScrollPane1 = new javax.swing.JScrollPane();
+      lblManagedOptionsTextArea = new javax.swing.JTextArea();
 
       setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -91,11 +104,23 @@ public class HelpDialog extends JDialog{
 
       lblReleaseDateOut.setText("...");
 
+      lblCopyrightOut.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
       lblCopyrightOut.setText("...");
 
       lblProductNameOut.setText("...");
 
       lblLogo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
+      lblManagedOptionsLabel.setText("Managed options:");
+
+      jScrollPane1.setEnabled(false);
+
+      lblManagedOptionsTextArea.setEditable(false);
+      lblManagedOptionsTextArea.setColumns(20);
+      lblManagedOptionsTextArea.setFont(new java.awt.Font("Tahoma", 0, 11)); // NOI18N
+      lblManagedOptionsTextArea.setRows(5);
+      lblManagedOptionsTextArea.setTabSize(3);
+      jScrollPane1.setViewportView(lblManagedOptionsTextArea);
 
       javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
       getContentPane().setLayout(layout);
@@ -103,29 +128,41 @@ public class HelpDialog extends JDialog{
          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
          .addGroup(layout.createSequentialGroup()
             .addContainerGap()
-            .addComponent(lblLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
-            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+               .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                  .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(lblProductNameOut, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(144, 144, 144))
+                     .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                           .addGroup(layout.createSequentialGroup()
+                              .addComponent(lblReleaseDate)
+                              .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                              .addComponent(lblReleaseDateOut, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                           .addGroup(layout.createSequentialGroup()
+                              .addComponent(lblProductVersion)
+                              .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                              .addComponent(lblProductVersionOut, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                  .addComponent(lblLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))
+               .addComponent(lblCopyrightOut, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                .addGroup(layout.createSequentialGroup()
-                  .addComponent(lblReleaseDate)
-                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                  .addComponent(lblReleaseDateOut, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-               .addComponent(lblProductNameOut, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-               .addGroup(layout.createSequentialGroup()
-                  .addComponent(lblProductVersion)
-                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                  .addComponent(lblProductVersionOut, javax.swing.GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE))
-               .addComponent(lblCopyrightOut, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                  .addComponent(lblManagedOptionsLabel)
+                  .addGap(0, 0, Short.MAX_VALUE))
+               .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                  .addGap(0, 0, Short.MAX_VALUE)
+                  .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 412, javax.swing.GroupLayout.PREFERRED_SIZE)))
             .addContainerGap())
       );
       layout.setVerticalGroup(
          layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
          .addGroup(layout.createSequentialGroup()
             .addContainerGap()
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                .addGroup(layout.createSequentialGroup()
                   .addComponent(lblProductNameOut)
-                  .addGap(18, 18, 18)
+                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                   .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                      .addComponent(lblProductVersion)
                      .addComponent(lblProductVersionOut))
@@ -133,9 +170,14 @@ public class HelpDialog extends JDialog{
                   .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                      .addComponent(lblReleaseDate)
                      .addComponent(lblReleaseDateOut))
-                  .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                  .addComponent(lblCopyrightOut))
+                  .addGap(75, 75, 75))
                .addComponent(lblLogo, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(lblManagedOptionsLabel)
+            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGap(18, 18, 18)
+            .addComponent(lblCopyrightOut)
             .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
       );
 
@@ -188,8 +230,11 @@ public class HelpDialog extends JDialog{
 	}
 
    // Variables declaration - do not modify//GEN-BEGIN:variables
+   private javax.swing.JScrollPane jScrollPane1;
    private javax.swing.JLabel lblCopyrightOut;
    private javax.swing.JLabel lblLogo;
+   private javax.swing.JLabel lblManagedOptionsLabel;
+   private javax.swing.JTextArea lblManagedOptionsTextArea;
    private javax.swing.JLabel lblProductNameOut;
    private javax.swing.JLabel lblProductVersion;
    private javax.swing.JLabel lblProductVersionOut;
