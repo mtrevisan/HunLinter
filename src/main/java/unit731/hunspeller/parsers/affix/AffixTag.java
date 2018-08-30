ca5d7f1dea13451835d6532cf5d17eb0e7f68838
Mauro Trevisan <mauro.simioni@targaubiest.com>
2018-08-30 15:57:33 +0200

    no message

--- src/main/java/unit731/hunspeller/parsers/affix/AffixTag.java
+++ src/main/java/unit731/hunspeller/parsers/affix/AffixTag.java
@@ -35,12 +35,14 @@ public enum AffixTag{
 //	TRY("TRY"),
 	/** Words signed with this flag are not suggested (but still accepted when typed correctly) */
 //	NO_SUGGEST("NOSUGGEST"),
-	/** Similar to NOSUGGEST, but it forbids to use the word in n-gram based (more, than 1-character distance) suggestions */
-//	NO_NGRAM_SUGGEST("NONGRAMSUGGEST"),
 //	MAX_COMPOUND_SUGGEST("MAXCPDSUGS"),
 //	MAX_NGRAM_SUGGEST("MAXNGRAMSUGS"),
 //	MAX_NGRAM_SIMILARITY_FACTOR("MAXDIFF"),
 //	ONLY_MAX_NGRAM_SIMILARITY_FACTOR("ONLYMAXDIFF"),
+	/** Sable word suggestions with spaces */
+//	NO_SPLIT_SUGGEST("NOSPLITSUGS"),
+	/** Similar to NOSUGGEST, but it forbids to use the word in n-gram based (more, than 1-character distance) suggestions */
+//	NO_NGRAM_SUGGEST("NONGRAMSUGGEST"),
 //	SUGGESTIONS_WITH_DOTS("SUGSWITHDOTS"),
 	/** If space is used then all the words must be present in the dictionary */
 	REPLACEMENT_TABLE("REP"),
@@ -69,7 +71,8 @@ public enum AffixTag{
 	/** Words signed with this flag (or with a signed affix) may be middle elements in compound words */
 //	COMPOUND_MIDDLE("COMPOUNDMIDDLE"),
 	/** Words signed with this flag (or with a signed affix) may be last elements in compound words */
-//	COMPOUND_END("COMPOUNDEND"),
+//	COMPOUND_END("COMPOUNDEND"),?
+//	COMPOUND_LAST("COMPOUNDLAST"),?
 	/** Suffixes signed this flag may be only inside of compounds (this flag works also with words) */
 	ONLY_IN_COMPOUND("ONLYINCOMPOUND"),
 	/**
@@ -77,10 +80,12 @@ public enum AffixTag{
 	 * at the end of compounds only).
 	 */
 	COMPOUND_PERMIT_FLAG("COMPOUNDPERMITFLAG"),
-	/** Allow twofold suffixes within compounds */
-	COMPOUND_MORE_SUFFIXES("COMPOUNDMORESUFFIXES"),
 	/** Affixes with this flag forbid compounding of the affixed word */
 	COMPOUND_FORBID_FLAG("COMPOUNDFORBIDFLAG"),
+	/** Allow twofold suffixes within compounds */
+	COMPOUND_MORE_SUFFIXES("COMPOUNDMORESUFFIXES"),
+	/** Signs the compounds in the dictionary */
+//	COMPOUND_ROOT("COMPOUNDROOT"),
 	/** Set maximum word count in a compound word (default is unlimited) */
 	COMPOUND_WORD_MAX("COMPOUNDWORDMAX"),
 	/** Forbid word duplication in compounds */
@@ -96,31 +101,51 @@ public enum AffixTag{
 	CHECK_COMPOUND_TRIPLE("CHECKCOMPOUNDTRIPLE"),
 	/** Allow simplified 2-letter forms of the compounds forbidden by CHECKCOMPOUNDTRIPLE (Schiff|fahrt -> Schiffahrt) */
 	SIMPLIFIED_TRIPLE("SIMPLIFIEDTRIPLE"),
-	/** Affixes signed with this flag may be on a word when this word also has a prefix with CIRCUMFIX flag and vice versa */
-	CIRCUMFIX("CIRCUMFIX"),
-	/** Signs forbidden word form (affixed forms are also forbidden, excepts root homonyms) */
-	FORBIDDEN_WORD("FORBIDDENWORD"),
+	/**
+	 * Forbid compounding, if the first word in the compound ends with endchars, and next word begins with beginchars and (optionally) they have the requested flags. The optional replacement parameter allows simplified compound form.
+	 * The special "endchars" pattern 0 (zero) limits the rule to the unmodified stems (stems and stems with zero affixes)
+	 * Note: COMPOUNDMIN doesn't work correctly with the compound word alternation, so it may need to set COMPOUNDMIN to lower value.
+	 */
+//	CHECK_COMPOUND_PATTERN("CHECKCOMPOUNDPATTERN"),
+	/**
+	 * Last word part of a compound with flag FORCEUCASE forces capitalization of the whole compound word.
+	 * Eg. Dutch word "straat" (street) with FORCEUCASE flags will allowed only in capitalized compound forms, according to the Dutch spelling rules for proper names.
+	 */
+//	FORCE_UPPERCASE("FORCEUCASE"),
+	/** Needed for special compounding rules in Hungarian. First parameter is the maximum syllable number, that may be in a compound, if words in compounds are more than COMPOUNDWORDMAX. Second parameter is the list of vowels (for calculating syllables) */
+//	COMPOUND_SYLLABLE("COMPOUNDSYLLABLE"),
+	/** Needed for special compounding rules in Hungarian */
+//	SYLLABLE_NUMBER("SYLLABLENUM"),
 
 	//Options for affix creation
 	PREFIX("PFX"),
 	SUFFIX("SFX"),
 
 	//Other options
+	/** Affixes signed with this flag may be on a word when this word also has a prefix with CIRCUMFIX flag and vice versa */
+	CIRCUMFIX("CIRCUMFIX"),
+	/** Signs forbidden word form (affixed forms are also forbidden, excepts root homonyms) */
+	FORBIDDEN_WORD("FORBIDDENWORD"),
 	/** With this flag the affix rules can strip full words, not only one less characters */
 	FULLSTRIP("FULLSTRIP"),
 	/** Forbid uppercased and capitalized forms of words signed with this flag */
 	KEEP_CASE("KEEPCASE"),
+	/** Define input conversion table */
+	INPUT_CONVERSION_TABLE("ICONV"),
+	/** Define output conversion table */
+	OUTPUT_CONVERSION_TABLE("OCONV"),
 	/**
 	 * Signs virtual stems in the dictionary, words are valid only when affixed, except if the dictionary word has a homonym or a zero affix
 	 * (it works also with prefixes and prefix + suffix combinations)
 	 */
-	NEED_AFFIX("NEEDAFFIX"),
+	NEED_AFFIX("NEEDAFFIX");
+	/** Signs affix rules and dictionary words (allomorphs) not used in morphological generation and root words removed from suggestion */
+//	SUB_STANDARD("SUBSTANDARD"),
 	/** Extends tokenizer of Hunspell command line interface with additional word character */
 //	WORD_CHARS("WORDCHARS"),
-	/** Define input conversion table */
-	INPUT_CONVERSION_TABLE("ICONV"),
-	/** Define output conversion table */
-	OUTPUT_CONVERSION_TABLE("OCONV");
+	/** SS letter pair in uppercased (German) words may be upper case sharp s (ß). Hunspell can handle this special casing with the CHECKSHARPS declaration (see also KEEPCASE flag and tests/germancompounding example) in both spelling and suggestion */
+//	CHECK_SHARPS("CHECKSHARPS");
+
 
 
 	private final String code;
