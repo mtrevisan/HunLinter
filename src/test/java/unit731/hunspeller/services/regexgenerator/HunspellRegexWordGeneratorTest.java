bc97c5893b60eff104d4c75f91d806d4b72cfcdf
Mauro Trevisan <mauro.trevisan@gmail.com>
2018-09-09 19:40:48 +0200

    no message

--- src/test/java/unit731/hunspeller/services/regexgenerator/HunspellRegexWordGeneratorTest.java
+++ src/test/java/unit731/hunspeller/services/regexgenerator/HunspellRegexWordGeneratorTest.java
@@ -7,35 +7,24 @@ import org.junit.Test;
 
 public class HunspellRegexWordGeneratorTest{
 
-	//TODO
-//	@Test
-//	public void shouldGenerateAllWords(){
-//		String regex = "[abc]c[de]?";
-//
-//		HunspellRegexWordGenerator generator = new HunspellRegexWordGenerator(regex);
-//		List<String> words = generator.generateAll(10);
-//
-//		Assert.assertEquals(9l, words.size());
-//	}
-//
-//	@Test
-//	public void shouldGenerateEmptyWord(){
-//		String regex = "a?b?c?";
-//
-//		HunspellRegexWordGenerator generator = new HunspellRegexWordGenerator(regex);
-//		boolean infinite = generator.isInfinite();
-//
-//		Assert.assertFalse(infinite);
-//	}
-//
-//	@Test
-//	public void shouldNotGenerateEmptyWord(){
-//		String regex = "a?b?c?";
-//
-//		HunspellRegexWordGenerator generator = new HunspellRegexWordGenerator(regex);
-//		boolean infinite = generator.isInfinite();
-//
-//		Assert.assertFalse(infinite);
-//	}
+	@Test
+	public void shouldGenerateAllWords(){
+		String regex = "[abc]c[de]?";
+
+		HunspellRegexWordGenerator generator = new HunspellRegexWordGenerator(regex);
+		List<String> words = generator.generateAll(10);
+
+		Assert.assertEquals(9l, words.size());
+	}
+
+	@Test
+	public void shouldNotGenerateEmptyWord(){
+		String regex = "a?b?c?";
+
+		HunspellRegexWordGenerator generator = new HunspellRegexWordGenerator(regex);
+		List<String> words = generator.generateAll(10);
+
+		Assert.assertFalse(words.isEmpty());
+	}
 
 }
