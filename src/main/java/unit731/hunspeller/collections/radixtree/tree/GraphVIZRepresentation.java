1357c200c5cc7e99e1db45a8a15542b843b01792
Mauro Trevisan <mauro.trevisan@gmail.com>
2019-01-01 13:11:18 +0100

    extraction of some methods

---
+++ src/main/java/unit731/hunspeller/collections/radixtree/tree/GraphVIZRepresentation.java
@@ -0,0 +1,95 @@
+package unit731.hunspeller.collections.radixtree.tree;
+
+import java.io.Serializable;
+
+
+public class GraphVIZRepresentation{
+
+	private static final String GRAPHVIZ_STYLE_BEGIN = " [";
+	private static final String GRAPHVIZ_STYLE_END = "];";
+	private static final String GRAPHVIZ_STYLE_STRING_BOUNDARY = "\"";
+	private static final String GRAPHVIZ_ATTRIBUTE_SEPARATOR = ", ";
+	private static final char GRAPHVIZ_TAB = '\t';
+	private static final char GRAPHVIZ_NEW_LINE = '\n';
+	private static final String GRAPHVIZ_STYLE_ARROW = " -> ";
+	private static final String GRAPHVIZ_STYLE_LABEL = "label=";
+	private static final String GRAPHVIZ_STYLE_SHAPE = "shape=";
+	private static final String GRAPHVIZ_STYLE_FAILURE_TRANSITION = GRAPHVIZ_STYLE_BEGIN + "style=dashed, color=gray, constraint=false"
+		+ GRAPHVIZ_STYLE_END;
+	private static final String GRAPHVIZ_STYLE_STATE_WITHOUT_OUTPUT = GRAPHVIZ_STYLE_BEGIN + GRAPHVIZ_STYLE_SHAPE + "circle"
+		+ GRAPHVIZ_ATTRIBUTE_SEPARATOR + GRAPHVIZ_STYLE_LABEL + GRAPHVIZ_STYLE_STRING_BOUNDARY + GRAPHVIZ_STYLE_STRING_BOUNDARY
+		+ GRAPHVIZ_STYLE_END;
+	private static final String GRAPHVIZ_STYLE_STATE_WITH_OUTPUT_PRE_LABEL = GRAPHVIZ_STYLE_BEGIN + GRAPHVIZ_STYLE_SHAPE + "doublecircle"
+		+ GRAPHVIZ_ATTRIBUTE_SEPARATOR + GRAPHVIZ_STYLE_LABEL + GRAPHVIZ_STYLE_STRING_BOUNDARY;
+	private static final String GRAPHVIZ_STYLE_STATE_WITH_OUTPUT_POST_LABEL = GRAPHVIZ_STYLE_STRING_BOUNDARY + GRAPHVIZ_STYLE_END;
+
+
+	/**
+	 * @see <a href="http://www.webgraphviz.com/">GraphVIZ</a>
+	 * 
+	 * @param tree	The tree for which to extract the Graphviz representation
+	 * @param displayEdgesToInitialState	Whether to include the failure edges directing to the root node
+	 * @return	The GraphVIZ representation of this tree
+	 *
+	 * @param <S>	The sequence/key type
+	 * @param <V>	The type of values stored in the tree
+	 */
+	public <S, V extends Serializable> String generateGraphVIZRepresentation(RadixTree<S, V> tree, boolean displayEdgesToInitialState){
+		StringBuilder sb = new StringBuilder(40);
+		sb.append("digraph automaton{")
+			.append(GRAPHVIZ_NEW_LINE)
+			.append(GRAPHVIZ_TAB)
+			.append("graph [rankdir=LR];")
+			.append(GRAPHVIZ_NEW_LINE);
+
+		RadixTreeTraverser<S, V> traverserNode = (wholeKey, node, parent) -> graphVIZAppendNode(sb, node);
+		graphVIZAppendNode(sb, tree.root);
+		tree.traverseBFS(traverserNode);
+
+		RadixTreeTraverser<S, V> traverserForward = (wholeKey, node, parent) -> graphVIZAppendForwardTransition(sb, node, parent);
+		tree.traverseBFS(traverserForward);
+
+		if(tree.isAhoCorasickTree()){
+			RadixTreeTraverser<S, V> traverserFailure = (wholeKey, node, parent) -> graphVIZAppendFailureTransitions(tree, sb, node,
+				displayEdgesToInitialState);
+			tree.traverseBFS(traverserFailure);
+		}
+
+		sb.append("}");
+		return sb.toString();
+	}
+
+	private <S, V extends Serializable> void graphVIZAppendForwardTransition(StringBuilder sb, RadixTreeNode<S, V> node, RadixTreeNode<S, V> parent){
+		sb.append(GRAPHVIZ_TAB)
+			.append(System.identityHashCode(parent))
+			.append(GRAPHVIZ_STYLE_ARROW)
+			.append(System.identityHashCode(node))
+			.append(GRAPHVIZ_STYLE_BEGIN)
+			.append(GRAPHVIZ_STYLE_LABEL)
+			.append(GRAPHVIZ_STYLE_STRING_BOUNDARY)
+			.append(node.getKey())
+			.append(GRAPHVIZ_STYLE_STRING_BOUNDARY)
+			.append(GRAPHVIZ_STYLE_END)
+			.append(GRAPHVIZ_NEW_LINE);
+	}
+
+	private <S, V extends Serializable> void graphVIZAppendFailureTransitions(RadixTree<S, V> tree, StringBuilder sb, RadixTreeNode<S, V> node,
+			boolean displayEdgesToInitialState){
+		if(displayEdgesToInitialState || node.getFailNode() != tree.root || node == tree.root)
+			sb.append(GRAPHVIZ_TAB)
+				.append(System.identityHashCode(node))
+				.append(GRAPHVIZ_STYLE_ARROW)
+				.append(System.identityHashCode(node.getFailNode()))
+				.append(GRAPHVIZ_STYLE_FAILURE_TRANSITION)
+				.append(GRAPHVIZ_NEW_LINE);
+	}
+
+	private <S, V extends Serializable> void graphVIZAppendNode(StringBuilder sb, RadixTreeNode<S, V> node){
+		sb.append(GRAPHVIZ_TAB)
+			.append(System.identityHashCode(node))
+			.append(node.hasValue()? GRAPHVIZ_STYLE_STATE_WITH_OUTPUT_PRE_LABEL + node.getValue() + GRAPHVIZ_STYLE_STATE_WITH_OUTPUT_POST_LABEL:
+				GRAPHVIZ_STYLE_STATE_WITHOUT_OUTPUT)
+			.append(GRAPHVIZ_NEW_LINE);
+	}
+	
+}
--- src/main/java/unit731/hunspeller/collections/radixtree/tree/RadixTree.java
+++ src/main/java/unit731/hunspeller/collections/radixtree/tree/RadixTree.java
@@ -39,25 +39,6 @@ import unit731.hunspeller.collections.radixtree.sequencers.SequencerInterface;
  */
 public class RadixTree<S, V extends Serializable> implements Map<S, V>{
 
-	private static final String GRAPHVIZ_STYLE_BEGIN = " [";
-	private static final String GRAPHVIZ_STYLE_END = "];";
-	private static final String GRAPHVIZ_STYLE_STRING_BOUNDARY = "\"";
-	private static final String GRAPHVIZ_ATTRIBUTE_SEPARATOR = ", ";
-	private static final char GRAPHVIZ_TAB = '\t';
-	private static final char GRAPHVIZ_NEW_LINE = '\n';
-	private static final String GRAPHVIZ_STYLE_ARROW = " -> ";
-	private static final String GRAPHVIZ_STYLE_LABEL = "label=";
-	private static final String GRAPHVIZ_STYLE_SHAPE = "shape=";
-	private static final String GRAPHVIZ_STYLE_FAILURE_TRANSITION = GRAPHVIZ_STYLE_BEGIN + "style=dashed, color=gray, constraint=false"
-		+ GRAPHVIZ_STYLE_END;
-	private static final String GRAPHVIZ_STYLE_STATE_WITHOUT_OUTPUT = GRAPHVIZ_STYLE_BEGIN + GRAPHVIZ_STYLE_SHAPE + "circle"
-		+ GRAPHVIZ_ATTRIBUTE_SEPARATOR + GRAPHVIZ_STYLE_LABEL + GRAPHVIZ_STYLE_STRING_BOUNDARY + GRAPHVIZ_STYLE_STRING_BOUNDARY
-		+ GRAPHVIZ_STYLE_END;
-	private static final String GRAPHVIZ_STYLE_STATE_WITH_OUTPUT_PRE_LABEL = GRAPHVIZ_STYLE_BEGIN + GRAPHVIZ_STYLE_SHAPE + "doublecircle"
-		+ GRAPHVIZ_ATTRIBUTE_SEPARATOR + GRAPHVIZ_STYLE_LABEL + GRAPHVIZ_STYLE_STRING_BOUNDARY;
-	private static final String GRAPHVIZ_STYLE_STATE_WITH_OUTPUT_POST_LABEL = GRAPHVIZ_STYLE_STRING_BOUNDARY + GRAPHVIZ_STYLE_END;
-
-
 	private class TraverseElement{
 
 		protected final RadixTreeNode<S, V> node;
@@ -786,70 +767,6 @@ public class RadixTree<S, V extends Serializable> implements Map<S, V>{
 					stack.push(new VisitElement(child, node, newPrefix));
 			}
 		}
-	}
-
-
-	/**
-	 * @see <a href="http://www.webgraphviz.com/">GraphVIZ</a>
-	 * 
-	 * @param displayEdgesToInitialState	Whether to include the failure edges directing to the root node
-	 * @return	The GraphVIZ representation of this tree
-	 */
-	public String generateGraphvizRepresentation(boolean displayEdgesToInitialState){
-		StringBuilder sb = new StringBuilder(40);
-		sb.append("digraph automaton{")
-			.append(GRAPHVIZ_NEW_LINE)
-			.append(GRAPHVIZ_TAB)
-			.append("graph [rankdir=LR];")
-			.append(GRAPHVIZ_NEW_LINE);
-
-		RadixTreeTraverser<S, V> traverserNode = (wholeKey, node, parent) -> graphvizAppendNode(sb, node);
-		graphvizAppendNode(sb, root);
-		traverseBFS(traverserNode);
-
-		RadixTreeTraverser<S, V> traverserForward = (wholeKey, node, parent) -> graphvizAppendForwardTransition(sb, node, parent);
-		traverseBFS(traverserForward);
-
-		if(prepared){
-			RadixTreeTraverser<S, V> traverserFailure = (wholeKey, node, parent) -> graphvizAppendFailureTransitions(sb, node,
-				displayEdgesToInitialState);
-			traverseBFS(traverserFailure);
-		}
-
-		sb.append("}");
-		return sb.toString();
-	}
-
-	private void graphvizAppendForwardTransition(StringBuilder sb, RadixTreeNode<S, V> node, RadixTreeNode<S, V> parent){
-		sb.append(GRAPHVIZ_TAB)
-			.append(System.identityHashCode(parent))
-			.append(GRAPHVIZ_STYLE_ARROW)
-			.append(System.identityHashCode(node))
-			.append(GRAPHVIZ_STYLE_BEGIN)
-			.append(GRAPHVIZ_STYLE_LABEL)
-			.append(GRAPHVIZ_STYLE_STRING_BOUNDARY)
-			.append(node.getKey())
-			.append(GRAPHVIZ_STYLE_STRING_BOUNDARY)
-			.append(GRAPHVIZ_STYLE_END)
-			.append(GRAPHVIZ_NEW_LINE);
-	}
-
-	private void graphvizAppendFailureTransitions(StringBuilder sb, RadixTreeNode<S, V> node, boolean displayEdgesToInitialState){
-		if(displayEdgesToInitialState || node.getFailNode() != root || node == root)
-			sb.append(GRAPHVIZ_TAB)
-				.append(System.identityHashCode(node))
-				.append(GRAPHVIZ_STYLE_ARROW)
-				.append(System.identityHashCode(node.getFailNode()))
-				.append(GRAPHVIZ_STYLE_FAILURE_TRANSITION)
-				.append(GRAPHVIZ_NEW_LINE);
-	}
-
-	private void graphvizAppendNode(StringBuilder sb, RadixTreeNode<S, V> node){
-		sb.append(GRAPHVIZ_TAB)
-			.append(System.identityHashCode(node))
-			.append(node.hasValue()? GRAPHVIZ_STYLE_STATE_WITH_OUTPUT_PRE_LABEL + node.getValue() + GRAPHVIZ_STYLE_STATE_WITH_OUTPUT_POST_LABEL:
-				GRAPHVIZ_STYLE_STATE_WITHOUT_OUTPUT)
-			.append(GRAPHVIZ_NEW_LINE);
 	}
 
 }
