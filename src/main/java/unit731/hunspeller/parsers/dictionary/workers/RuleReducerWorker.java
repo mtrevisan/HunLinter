8b37662dc6c41fa9cb24b26a1b2f24f2b500c297
Mauro Trevisan <mauro.trevisan@gmail.com>
2019-01-13 15:44:48 +0100

    no message

--- src/main/java/unit731/hunspeller/parsers/dictionary/workers/RuleReducerWorker.java
+++ src/main/java/unit731/hunspeller/parsers/dictionary/workers/RuleReducerWorker.java
@@ -1,21 +1,12 @@
 package unit731.hunspeller.parsers.dictionary.workers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.function.BiConsumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.slf4j.Logger;
@@ -90,7 +81,6 @@ public class RuleReducerWorker extends WorkerDictionaryBase{
 	}
 
 
-	/** NOTE: this works only if the rules produce only one output! ... for now */
 	public RuleReducerWorker(AffixData affixData, DictionaryParser dicParser, WordGenerator wordGenerator){
 		Objects.requireNonNull(affixData);
 		Objects.requireNonNull(wordGenerator);
@@ -263,7 +253,7 @@ System.out.println("");
 	}
 
 	private List<LineEntry> reduceEntriesToRules(Map<String, List<LineEntry>> aggregatedFlaggedEntries){
-		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+		//TODO
 	}
 
 //	private void manageCollision(LineEntry affixEntry, Set<LineEntry> newAffixEntries){
@@ -290,167 +280,167 @@ System.out.println("");
 //			}
 //	}
 
-	private List<LineEntry> collectEntries(LineEntry affixEntry, List<LineEntry> entries){
-		//collect all the entries that have affixEntry as last part of the condition
-		String affixEntryCondition = affixEntry.condition;
-		Set<LineEntry> collisions = new HashSet<>();
-		collisions.add(affixEntry);
-		for(int i = 1; i < entries.size(); i ++){
-			LineEntry targetAffixEntry = entries.get(i);
-			String targetAffixEntryCondition = targetAffixEntry.condition;
-			if(targetAffixEntryCondition.endsWith(affixEntryCondition))
-				collisions.add(new LineEntry(targetAffixEntry.removal, targetAffixEntry.addition, targetAffixEntryCondition));
-		}
-		return new ArrayList<>(collisions);
-	}
+//	private List<LineEntry> collectEntries(LineEntry affixEntry, List<LineEntry> entries){
+//		//collect all the entries that have affixEntry as last part of the condition
+//		String affixEntryCondition = affixEntry.condition;
+//		Set<LineEntry> collisions = new HashSet<>();
+//		collisions.add(affixEntry);
+//		for(int i = 1; i < entries.size(); i ++){
+//			LineEntry targetAffixEntry = entries.get(i);
+//			String targetAffixEntryCondition = targetAffixEntry.condition;
+//			if(targetAffixEntryCondition.endsWith(affixEntryCondition))
+//				collisions.add(new LineEntry(targetAffixEntry.removal, targetAffixEntry.addition, targetAffixEntryCondition));
+//		}
+//		return new ArrayList<>(collisions);
+//	}
 
-	private void joinCollisions(List<LineEntry> startingList, List<LineEntry> nextList, Comparator<String> comparator){
-		//extract the prior-to-last letter
-		int size = startingList.size();
-		for(int i = 0; i < size; i ++){
-			LineEntry affixEntry = startingList.get(i);
-			String affixEntryCondition = affixEntry.condition;
-			String[] startingCondition = RegExpSequencer.splitSequence(affixEntryCondition);
-			int discriminatorIndex = startingCondition.length;
-			String affixEntryRemoval = affixEntry.removal;
-			String affixEntryAddition = affixEntry.addition;
-			//strip affixEntry's condition and collect
-			List<String> otherConditions = nextList.stream()
-				.map(entry -> entry.condition)
-				.map(RegExpSequencer::splitSequence)
-				.filter(condition -> SEQUENCER.endsWith(condition, startingCondition))
-				.map(condition -> condition[condition.length - discriminatorIndex])
-				.distinct()
-				.map(String::valueOf)
-				.collect(Collectors.toList());
-			if(!otherConditions.isEmpty()){
-				Stream<String> other;
-				//if this condition.length > startingCondition.length + 1, then add in-between rules
-				if(discriminatorIndex > 0 && discriminatorIndex + 1 == SEQUENCER.length(startingCondition)){
-					//collect intermediate letters
-					Collection<String> letterBucket = bucketForLetter(nextList, discriminatorIndex, comparator);
+//	private void joinCollisions(List<LineEntry> startingList, List<LineEntry> nextList, Comparator<String> comparator){
+//		//extract the prior-to-last letter
+//		int size = startingList.size();
+//		for(int i = 0; i < size; i ++){
+//			LineEntry affixEntry = startingList.get(i);
+//			String affixEntryCondition = affixEntry.condition;
+//			String[] startingCondition = RegExpSequencer.splitSequence(affixEntryCondition);
+//			int discriminatorIndex = startingCondition.length;
+//			String affixEntryRemoval = affixEntry.removal;
+//			String affixEntryAddition = affixEntry.addition;
+//			//strip affixEntry's condition and collect
+//			List<String> otherConditions = nextList.stream()
+//				.map(entry -> entry.condition)
+//				.map(RegExpSequencer::splitSequence)
+//				.filter(condition -> SEQUENCER.endsWith(condition, startingCondition))
+//				.map(condition -> condition[condition.length - discriminatorIndex])
+//				.distinct()
+//				.map(String::valueOf)
+//				.collect(Collectors.toList());
+//			if(!otherConditions.isEmpty()){
+//				Stream<String> other;
+//				//if this condition.length > startingCondition.length + 1, then add in-between rules
+//				if(discriminatorIndex > 0 && discriminatorIndex + 1 == SEQUENCER.length(startingCondition)){
+//					//collect intermediate letters
+//					Collection<String> letterBucket = bucketForLetter(nextList, discriminatorIndex, comparator);
+//
+//					for(String letter : letterBucket)
+//						startingList.add(new LineEntry(affixEntryRemoval, affixEntryAddition, letter));
+//
+//					//merge conditions
+//					Stream<String> startingConditionStream = Arrays.stream(startingCondition[discriminatorIndex - 1].substring(2,
+//						startingCondition[discriminatorIndex - 1].length() - 1).split(StringUtils.EMPTY));
+//					other = Stream.concat(startingConditionStream, otherConditions.stream())
+//						.distinct();
+//					affixEntryCondition = StringUtils.join(ArrayUtils.remove(startingCondition, 0));
+//				}
+//				else if(discriminatorIndex + 1 > SEQUENCER.length(startingCondition)){
+//					//collect intermediate letters
+//					Collection<String> letterBucket = bucketForLetter(nextList, discriminatorIndex, comparator);
+//
+//					for(String letter : letterBucket)
+//						startingList.add(new LineEntry(affixEntryRemoval, affixEntryAddition, letter));
+//
+//					other = otherConditions.stream();
+//				}
+//				else{
+//					other = otherConditions.stream();
+//				}
+//
+//				String otherCondition = other
+//					.sorted(comparator)
+//					.collect(Collectors.joining());
+//				startingList.set(i, new LineEntry(affixEntryRemoval, affixEntryAddition, NOT_GROUP_STARTING + otherCondition + GROUP_ENDING
+//					+ affixEntryCondition));
+//			}
+//		}
+//	}
 
-					for(String letter : letterBucket)
-						startingList.add(new LineEntry(affixEntryRemoval, affixEntryAddition, letter));
+//	private Map<Integer, List<LineEntry>> bucketForLength(List<LineEntry> entries, Comparator<String> comparator){
+//		Map<Integer, List<LineEntry>> bucket = new HashMap<>();
+//		for(LineEntry entry : entries){
+//			int entryLength = entry.condition.length() - (entry.condition.startsWith(NOT_GROUP_STARTING)? 3:
+//				(entry.condition.startsWith(GROUP_STARTING)? 2: 0));
+//			bucket.computeIfAbsent(entryLength, k -> new ArrayList<>())
+//				.add(entry);
+//		}
+//		//order lists
+//		Comparator<LineEntry> comp = (pair1, pair2) -> comparator.compare(pair1.condition, pair2.condition);
+//		for(Map.Entry<Integer, List<LineEntry>> bag : bucket.entrySet())
+//			bag.getValue().sort(comp);
+//		return bucket;
+//	}
 
-					//merge conditions
-					Stream<String> startingConditionStream = Arrays.stream(startingCondition[discriminatorIndex - 1].substring(2,
-						startingCondition[discriminatorIndex - 1].length() - 1).split(StringUtils.EMPTY));
-					other = Stream.concat(startingConditionStream, otherConditions.stream())
-						.distinct();
-					affixEntryCondition = StringUtils.join(ArrayUtils.remove(startingCondition, 0));
-				}
-				else if(discriminatorIndex + 1 > SEQUENCER.length(startingCondition)){
-					//collect intermediate letters
-					Collection<String> letterBucket = bucketForLetter(nextList, discriminatorIndex, comparator);
+//	private Collection<String> bucketForLetter(List<LineEntry> entries, int index, Comparator<String> comparator){
+//		//collect by letter at given index
+//		Map<String, String> bucket = new HashMap<>();
+//		for(LineEntry entry : entries){
+//			String condition = entry.condition;
+//			String key = String.valueOf(condition.charAt(index));
+//			String bag = bucket.get(key);
+//			if(bag != null)
+//				condition = Arrays.asList(bag.charAt(index - 1), condition.charAt(index - 1)).stream()
+//					.map(String::valueOf)
+//					.sorted(comparator)
+//					.collect(Collectors.joining(StringUtils.EMPTY, NOT_GROUP_STARTING, GROUP_ENDING))
+//					+ condition.substring(index);
+//			bucket.put(key, condition);
+//		}
+//
+//		//convert non-merged conditions
+//		List<String> valuesBucket = bucket.values().stream()
+//			.map(condition -> (condition.charAt(0) == '['? condition: NOT_GROUP_STARTING + condition.charAt(0) + GROUP_ENDING + condition.substring(index)))
+//			.collect(Collectors.toList());
+//		bucket.clear();
+//
+//		//merge non-merged conditions
+//		for(String condition : valuesBucket){
+//			int idx = condition.indexOf(GROUP_ENDING);
+//			String key = condition.substring(0, idx + 1);
+//			String bag = bucket.get(key);
+//			if(bag != null){
+//				String[] letters = (bag.charAt(idx + 1) == '['?
+//					bag.substring(idx + 2, bag.indexOf(']', idx + 2)).split(StringUtils.EMPTY):
+//					new String[]{String.valueOf(bag.charAt(idx + 1))});
+//				letters = ArrayUtils.add(letters, String.valueOf(condition.charAt(idx + 1)));
+//				condition = key
+//					+ Arrays.asList(letters).stream()
+//					.sorted(comparator)
+//					.collect(Collectors.joining(StringUtils.EMPTY, GROUP_STARTING, GROUP_ENDING))
+//					+ condition.substring(idx + 2);
+//			}
+//			bucket.put(key, condition);
+//		}
+//		return bucket.values();
+//	}
 
-					for(String letter : letterBucket)
-						startingList.add(new LineEntry(affixEntryRemoval, affixEntryAddition, letter));
+//	private String composeHeader(AffixEntry.Type type, String flag, boolean isCombineable, int size){
+//		StringBuilder sb = new StringBuilder();
+//		return sb.append(type.getFlag().getCode())
+//			.append(StringUtils.SPACE)
+//			.append(flag)
+//			.append(StringUtils.SPACE)
+//			.append(isCombineable? RuleEntry.COMBINEABLE: RuleEntry.NOT_COMBINEABLE)
+//			.append(StringUtils.SPACE)
+//			.append(size)
+//			.toString();
+//	}
 
-					other = otherConditions.stream();
-				}
-				else{
-					other = otherConditions.stream();
-				}
-
-				String otherCondition = other
-					.sorted(comparator)
-					.collect(Collectors.joining());
-				startingList.set(i, new LineEntry(affixEntryRemoval, affixEntryAddition, NOT_GROUP_STARTING + otherCondition + GROUP_ENDING
-					+ affixEntryCondition));
-			}
-		}
-	}
-
-	private Map<Integer, List<LineEntry>> bucketForLength(List<LineEntry> entries, Comparator<String> comparator){
-		Map<Integer, List<LineEntry>> bucket = new HashMap<>();
-		for(LineEntry entry : entries){
-			int entryLength = entry.condition.length() - (entry.condition.startsWith(NOT_GROUP_STARTING)? 3:
-				(entry.condition.startsWith(GROUP_STARTING)? 2: 0));
-			bucket.computeIfAbsent(entryLength, k -> new ArrayList<>())
-				.add(entry);
-		}
-		//order lists
-		Comparator<LineEntry> comp = (pair1, pair2) -> comparator.compare(pair1.condition, pair2.condition);
-		for(Map.Entry<Integer, List<LineEntry>> bag : bucket.entrySet())
-			bag.getValue().sort(comp);
-		return bucket;
-	}
-
-	private Collection<String> bucketForLetter(List<LineEntry> entries, int index, Comparator<String> comparator){
-		//collect by letter at given index
-		Map<String, String> bucket = new HashMap<>();
-		for(LineEntry entry : entries){
-			String condition = entry.condition;
-			String key = String.valueOf(condition.charAt(index));
-			String bag = bucket.get(key);
-			if(bag != null)
-				condition = Arrays.asList(bag.charAt(index - 1), condition.charAt(index - 1)).stream()
-					.map(String::valueOf)
-					.sorted(comparator)
-					.collect(Collectors.joining(StringUtils.EMPTY, NOT_GROUP_STARTING, GROUP_ENDING))
-					+ condition.substring(index);
-			bucket.put(key, condition);
-		}
-
-		//convert non-merged conditions
-		List<String> valuesBucket = bucket.values().stream()
-			.map(condition -> (condition.charAt(0) == '['? condition: NOT_GROUP_STARTING + condition.charAt(0) + GROUP_ENDING + condition.substring(index)))
-			.collect(Collectors.toList());
-		bucket.clear();
-
-		//merge non-merged conditions
-		for(String condition : valuesBucket){
-			int idx = condition.indexOf(GROUP_ENDING);
-			String key = condition.substring(0, idx + 1);
-			String bag = bucket.get(key);
-			if(bag != null){
-				String[] letters = (bag.charAt(idx + 1) == '['?
-					bag.substring(idx + 2, bag.indexOf(']', idx + 2)).split(StringUtils.EMPTY):
-					new String[]{String.valueOf(bag.charAt(idx + 1))});
-				letters = ArrayUtils.add(letters, String.valueOf(condition.charAt(idx + 1)));
-				condition = key
-					+ Arrays.asList(letters).stream()
-					.sorted(comparator)
-					.collect(Collectors.joining(StringUtils.EMPTY, GROUP_STARTING, GROUP_ENDING))
-					+ condition.substring(idx + 2);
-			}
-			bucket.put(key, condition);
-		}
-		return bucket.values();
-	}
-
-	private String composeHeader(AffixEntry.Type type, String flag, boolean isCombineable, int size){
-		StringBuilder sb = new StringBuilder();
-		return sb.append(type.getFlag().getCode())
-			.append(StringUtils.SPACE)
-			.append(flag)
-			.append(StringUtils.SPACE)
-			.append(isCombineable? RuleEntry.COMBINEABLE: RuleEntry.NOT_COMBINEABLE)
-			.append(StringUtils.SPACE)
-			.append(size)
-			.toString();
-	}
-
-	private String composeLine(AffixEntry.Type type, String flag, LineEntry partialLine){
-		StringBuilder sb = new StringBuilder();
-		sb.append(type.getFlag().getCode())
-			.append(StringUtils.SPACE)
-			.append(flag)
-			.append(StringUtils.SPACE)
-			.append(partialLine.removal);
-		int idx = partialLine.addition.indexOf(TAB);
-		if(idx >= 0)
-			sb.append(partialLine.addition.substring(0, idx))
-				.append(StringUtils.SPACE)
-				.append(partialLine.condition)
-				.append(TAB)
-				.append(partialLine.addition.substring(idx + 1));
-		else
-			sb.append(partialLine.addition)
-				.append(StringUtils.SPACE)
-				.append(partialLine.condition);
-		return sb.toString();
-	}
+//	private String composeLine(AffixEntry.Type type, String flag, LineEntry partialLine){
+//		StringBuilder sb = new StringBuilder();
+//		sb.append(type.getFlag().getCode())
+//			.append(StringUtils.SPACE)
+//			.append(flag)
+//			.append(StringUtils.SPACE)
+//			.append(partialLine.removal);
+//		int idx = partialLine.addition.indexOf(TAB);
+//		if(idx >= 0)
+//			sb.append(partialLine.addition.substring(0, idx))
+//				.append(StringUtils.SPACE)
+//				.append(partialLine.condition)
+//				.append(TAB)
+//				.append(partialLine.addition.substring(idx + 1));
+//		else
+//			sb.append(partialLine.addition)
+//				.append(StringUtils.SPACE)
+//				.append(partialLine.condition);
+//		return sb.toString();
+//	}
 
 }
