5b9b55e89f95b7b990282839068e58dc29db25b5
Mauro Trevisan <mauro.trevisan@gmail.com>
2019-02-12 22:41:25 +0100

    no message

--- src/main/java/unit731/hunspeller/languages/DictionaryCorrectnessChecker.java
+++ src/main/java/unit731/hunspeller/languages/DictionaryCorrectnessChecker.java
@@ -85,7 +85,7 @@ public class DictionaryCorrectnessChecker{
 		StringBuilder sb = new StringBuilder(e.getMessage());
 		if(production.hasProductionRules())
 			sb.append(" (via ").append(production.getRulesSequence()).append(")");
-		sb.append(" for ").append(production.getWord());
+		sb.append(" to ").append(production.getWord());
 		throw new IllegalArgumentException(sb.toString());
 	}
 
