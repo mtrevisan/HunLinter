b954ba71efcf34b9b7895810d5cc1f501a2e7c0f
Mauro Trevisan <mauro.simioni@targaubiest.com>
2019-03-28 13:47:59 +0100

    no message

--- src/main/java/unit731/hunspeller/collections/radixtree/dtos/TraverseElement.java
+++ src/main/java/unit731/hunspeller/collections/radixtree/dtos/TraverseElement.java
@@ -1,6 +1,8 @@
 package unit731.hunspeller.collections.radixtree.dtos;
 
 import java.io.Serializable;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import unit731.hunspeller.collections.radixtree.utils.RadixTreeNode;
 
 
@@ -21,6 +23,28 @@ public class TraverseElement<S, V extends Serializable>{
 
 	public S getPrefix(){
 		return prefix;
+	}
+
+	@Override
+	public boolean equals(Object obj){
+		if(obj == this)
+			return true;
+		if(obj == null || obj.getClass() != getClass())
+			return false;
+
+		TraverseElement<?, ?> rhs = (TraverseElement<?, ?>)obj;
+		return new EqualsBuilder()
+			.append(node, rhs.node)
+			.append(prefix, rhs.prefix)
+			.isEquals();
+	}
+
+	@Override
+	public int hashCode(){
+		return new HashCodeBuilder()
+			.append(node)
+			.append(prefix)
+			.toHashCode();
 	}
 
 }
--- src/main/java/unit731/hunspeller/collections/radixtree/dtos/VisitElement.java
+++ src/main/java/unit731/hunspeller/collections/radixtree/dtos/VisitElement.java
@@ -1,6 +1,8 @@
 package unit731.hunspeller.collections.radixtree.dtos;
 
 import java.io.Serializable;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import unit731.hunspeller.collections.radixtree.utils.RadixTreeNode;
 
 
@@ -17,6 +19,28 @@ public class VisitElement<S, V extends Serializable> extends TraverseElement<S, 
 
 	public RadixTreeNode<S, V> getParent(){
 		return parent;
+	}
+
+	@Override
+	public boolean equals(Object obj){
+		if(obj == this)
+			return true;
+		if(obj == null || obj.getClass() != getClass())
+			return false;
+
+		VisitElement<?, ?> rhs = (VisitElement<?, ?>)obj;
+		return new EqualsBuilder()
+			.appendSuper(super.equals(obj))
+			.append(parent, rhs.parent)
+			.isEquals();
+	}
+
+	@Override
+	public int hashCode(){
+		return new HashCodeBuilder()
+			.appendSuper(super.hashCode())
+			.append(parent)
+			.toHashCode();
 	}
 
 }
