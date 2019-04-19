fdf2ba09fee8039ef902f6571debd50038a218ea
Mauro Trevisan <mauro.trevisan@gmail.com>
2019-04-19 22:29:56 +0200

    no message

--- src/main/java/unit731/hunspeller/parsers/dictionary/vos/AffixEntry.java
+++ src/main/java/unit731/hunspeller/parsers/dictionary/vos/AffixEntry.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
