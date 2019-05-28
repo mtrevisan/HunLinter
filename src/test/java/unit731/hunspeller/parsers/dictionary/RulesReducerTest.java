f9ded470ffc0b22c6bbdae895c80af1a09c47c35
Mauro Trevisan <mauro.simioni@targaubiest.com>
2019-05-28 14:56:51 +0200

    no message

--- src/test/java/unit731/hunspeller/parsers/dictionary/RulesReducerTest.java
+++ src/test/java/unit731/hunspeller/parsers/dictionary/RulesReducerTest.java
@@ -232,36 +232,26 @@ public class RulesReducerTest{
 			"SET UTF-8",
 			"LANG vec",
 			"FLAG long",
-			"FULLSTRIP",
-			"SFX §0 Y 17",
-			"SFX §0 0 ato [nr]",
-			"SFX §0 èl elato èl",
-			"SFX §0 0 ta [^bklns]a",
-			"SFX §0 0 ato [^è]l",
-			"SFX §0 o ato [^bkmv]o",
-			"SFX §0 òba obata òba",
-			"SFX §0 òka okata òka",
-			"SFX §0 èla elata èla",
-			"SFX §0 òna onata òna",
-			"SFX §0 òsa osata òsa",
-			"SFX §0 òbo obato òbo",
-			"SFX §0 òko okato òko",
-			"SFX §0 òmo omato òmo",
-			"SFX §0 òvo ovato òvo",
-			"SFX §0 0 ta [^è]la",
-			"SFX §0 o ato [^ò]ko",
-			"SFX §0 0 ta [^ò][kns]a"
+			"SFX v1 Y 10",
+			"SFX v1 e ista e",
+			"SFX v1 0 ista [ln]",
+			"SFX v1 ía ista ía",
+			"SFX v1 o sta io",
+			"SFX v1 èr erista èr",
+			"SFX v1 a ista [^dií]a",
+			"SFX v1 o ista [^i]o",
+			"SFX v1 0 ista [^è]r",
+			"SFX v1 òda odista òda",
+			"SFX v1 ònia onista ònia"
 		);
 		Pair<RulesReducer, WordGenerator> pair = createReducer(affFile);
 		RulesReducer reducer = pair.getLeft();
 		WordGenerator wordGenerator = pair.getRight();
-		String flag = "§0";
-		List<String> words = Arrays.asList("aria", "bar", "baron", "bon", "borso", "bosko", "dixnar", "dòna", "fakin", "far", "fator", "fatora", "grada", "granfo",
-			"gòba", "gòbo", "inkuixitor", "inkuixitora", "inspetor", "inspetora", "kalandra", "kalandro", "kanèl", "kapèl", "kapèla", "kara", "kojon", "konto",
-			"kora", "koɉon", "kuadra", "kuadro", "kòsa", "libro", "maca", "mando", "manxo", "manđo", "marenda", "merenda", "mol", "muso", "padron", "paron",
-			"patron", "pecenin", "pesenin", "peŧenin", "porko", "pòka", "pòko", "rexon", "rosiñol", "roxiñol", "rusiñol", "ruxiñol", "ròba", "savia", "savio", "sen",
-			"sinsin", "soko", "solfro", "sorgo", "speso", "sporko", "tabar", "toxa", "vedèl", "vedèla", "verdo", "vesin", "vexin", "vexo", "veŧin", "visio", "viŧio",
-			"xbir", "xel", "òka", "òko", "òmo", "òvo", "đeneral", "đilio", "ŧedro", "ŧinŧin", "ŧoko");
+		String flag = "v1";
+		List<String> words = Arrays.asList("folar", "foƚar", "spiƚorsar", "kaxo", "arte", "bonba", "dente", "dornal", "đornal", "fegura", "fogo", "kal", "kaƚo",
+			"kapital", "kapitaƚe", "kolor", "koƚor", "lexe", "ƚexe", "mòda", "paexe", "palaso", "palaŧo", "paƚaseto", "paƚaso", "real", "reaƚe", "stua", "xornal",
+			"xornaƚe", "bragièr", "figura", "boridon", "filoxomía", "fiƚoxomía", "alarme", "alkimía", "aƚarme", "arkimía", "bonton", "finoxomía", "kanbio",
+			"kitara", "konto", "ŧerimònia", "ŧifra", "bregièr");
 		List<String> originalLines = words.stream()
 			.map(word -> word + "/" + flag)
 			.collect(Collectors.toList());
@@ -272,50 +262,38 @@ public class RulesReducerTest{
 		List<RulesReducer.LineEntry> compactedRules = reducer.reduceProductions(originalRules);
 
 		List<RulesReducer.LineEntry> expectedCompactedRules = Arrays.asList(
-			new RulesReducer.LineEntry("òbo", "obato", "òbo", "gòbo"),
-			new RulesReducer.LineEntry("òba", "obata", "òba", Arrays.asList("gòba", "ròba")),
-			new RulesReducer.LineEntry("òko", "okato", "òko", Arrays.asList("pòko", "òko")),
-			new RulesReducer.LineEntry("èla", "elata", "èla", Arrays.asList("kapèla", "vedèla")),
-			new RulesReducer.LineEntry("òna", "onata", "òna", "dòna"),
-			new RulesReducer.LineEntry("òmo", "omato", "òmo", "òmo"),
-			new RulesReducer.LineEntry("òsa", "osata", "òsa", "kòsa"),
-			new RulesReducer.LineEntry("èl", "elato", "èl", Arrays.asList("vedèl", "kanèl", "kapèl")),
-			new RulesReducer.LineEntry("òka", "okata", "òka", Arrays.asList("òka", "pòka")),
-			new RulesReducer.LineEntry("òvo", "ovato", "òvo", "òvo"),
-			new RulesReducer.LineEntry("0", "ato", "[nr]", Arrays.asList("bon", "dixnar", "veŧin", "bar", "far", "tabar", "paron", "koɉon", "ŧinŧin", "inkuixitor",
-				"sen", "baron", "vexin", "patron", "peŧenin", "vesin", "pecenin", "xbir", "kojon", "rexon", "inspetor", "fator", "sinsin", "padron", "pesenin", "fakin")),
-			new RulesReducer.LineEntry("o", "ato", "[^bkmv]o", Arrays.asList("verdo", "libro", "đilio", "mando", "viŧio", "savio", "speso", "kalandro", "vexo",
-				"ŧedro", "konto", "manđo", "granfo", "sorgo", "visio", "muso", "borso", "manxo", "kuadro", "solfro")),
-			new RulesReducer.LineEntry("0", "ato", "[^è]l", Arrays.asList("rusiñol", "ruxiñol", "rosiñol", "mol", "đeneral", "roxiñol", "xel")),
-			new RulesReducer.LineEntry("0", "ta", "[^bklns]a", Arrays.asList("kalandra", "kora", "maca", "savia", "aria", "inkuixitora", "marenda", "kuadra",
-				"inspetora", "toxa", "grada", "merenda", "kara", "fatora")),
-			new RulesReducer.LineEntry("o", "ato", "[^ò]ko", Arrays.asList("bosko", "soko", "ŧoko", "porko", "sporko"))
+			new RulesReducer.LineEntry("òda", "odista", "òda", "mòda"),
+			new RulesReducer.LineEntry("e", "ista", "e", Arrays.asList("kapitaƚe", "alarme", "ƚexe", "lexe", "paexe", "xornaƚe", "aƚarme", "reaƚe", "dente", "arte")),
+			new RulesReducer.LineEntry("ía", "ista", "ía", Arrays.asList("finoxomía", "fiƚoxomía", "alkimía", "arkimía", "filoxomía")),
+			new RulesReducer.LineEntry("ònia", "onista", "ònia", "ŧerimònia"),
+			new RulesReducer.LineEntry("èr", "erista", "èr", Arrays.asList("bregièr", "bragièr")),
+			new RulesReducer.LineEntry("0", "ista", "[ln]", Arrays.asList("dornal", "kal", "bonton", "đornal", "xornal", "real", "boridon", "kapital")),
+			new RulesReducer.LineEntry("o", "ista", "[^i]o", Arrays.asList("fogo", "konto", "paƚaso", "palaŧo", "kaƚo", "palaso", "paƚaseto", "kaxo")),
+			new RulesReducer.LineEntry("o", "sta", "io", "kanbio"),
+			new RulesReducer.LineEntry("a", "ista", "[^dií]a", Arrays.asList("fegura", "figura", "ŧifra", "bonba", "stua", "kitara")),
+			new RulesReducer.LineEntry("0", "ista", "[^è]r", Arrays.asList("folar", "koƚor", "foƚar", "spiƚorsar", "kolor"))
 		);
 		Assertions.assertEquals(expectedCompactedRules, compactedRules);
 
 		List<String> rules = reducer.convertFormat(flag, false, compactedRules);
 		List<String> expectedRules = Arrays.asList(
-			"SFX §0 Y 15",
-			"SFX §0 0 ato [nr]",
-			"SFX §0 èl elato èl",
-			"SFX §0 0 ta [^bklns]a",
-			"SFX §0 0 ato [^è]l",
-			"SFX §0 o ato [^bkmv]o",
-			"SFX §0 òba obata òba",
-			"SFX §0 òka okata òka",
-			"SFX §0 èla elata èla",
-			"SFX §0 òna onata òna",
-			"SFX §0 òsa osata òsa",
-			"SFX §0 òbo obato òbo",
-			"SFX §0 òko okato òko",
-			"SFX §0 òmo omato òmo",
-			"SFX §0 òvo ovato òvo",
-			"SFX §0 o ato [^ò]ko"
+			"SFX v1 Y 10",
+			"SFX v1 e ista e",
+			"SFX v1 0 ista [ln]",
+			"SFX v1 o sta io",
+			"SFX v1 ía ista ía",
+			"SFX v1 èr erista èr",
+			"SFX v1 a ista [^dií]a",
+			"SFX v1 o ista [^i]o",
+			"SFX v1 0 ista [^è]r",
+			"SFX v1 òda odista òda",
+			"SFX v1 ònia onista ònia"
 		);
 		Assertions.assertEquals(expectedRules, rules);
 
 		reducer.checkReductionCorrectness(flag, rules, originalRules, originalLines);
 	}
+
 
 	private Pair<RulesReducer, WordGenerator> createReducer(File affFile) throws IOException{
 		AffixParser affParser = new AffixParser();
