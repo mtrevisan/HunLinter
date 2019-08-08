ba72b24a8f205289d5b69f8a685e36c4ffe4c938
Mauro Trevisan <mauro.simioni@targaubiest.com>
2019-08-08 14:43:24 +0200

    no message

--- src/main/java/unit731/hunspeller/parsers/thesaurus/ThesaurusParser.java
+++ src/main/java/unit731/hunspeller/parsers/thesaurus/ThesaurusParser.java
@@ -29,6 +29,12 @@ import unit731.hunspeller.services.memento.CaretakerInterface;
 import unit731.hunspeller.services.memento.OriginatorInterface;
 
 
+/**
+ * for storing mementoes:
+ * https://github.com/dnaumenko/java-diff-utils
+ * https://www.adictosaltrabajo.com/2012/06/05/comparar-ficheros-java-diff-utils/
+ * https://github.com/java-diff-utils/java-diff-utils/wiki/Examples
+ */
 public class ThesaurusParser implements OriginatorInterface<ThesaurusParser.Memento>{
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ThesaurusParser.class);
