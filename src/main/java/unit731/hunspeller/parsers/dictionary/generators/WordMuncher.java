08663ad1f3d7f051ecadfd309f99403c8c4c2214
Mauro Trevisan <mauro.simioni@targaubiest.com>
2019-10-07 10:31:21 +0200

    no message

---
+++ src/main/java/unit731/hunspeller/parsers/dictionary/generators/WordMuncher.java
@@ -0,0 +1,184 @@
+package unit731.hunspeller.parsers.dictionary.generators;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import unit731.hunspeller.parsers.affix.AffixData;
+import unit731.hunspeller.parsers.affix.AffixParser;
+import unit731.hunspeller.parsers.dictionary.DictionaryParser;
+import unit731.hunspeller.parsers.enums.MorphologicalTag;
+import unit731.hunspeller.parsers.vos.AffixEntry;
+import unit731.hunspeller.parsers.vos.DictionaryEntry;
+import unit731.hunspeller.parsers.vos.Production;
+import unit731.hunspeller.parsers.vos.RuleEntry;
+import unit731.hunspeller.services.FileHelper;
+import unit731.hunspeller.services.SetHelper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+
+//https://github.com/nuspell/nuspell/blob/45d383c0e2f25e4ea48ee8efeca53c2bb51a3510/src/tools/munch.cxx
+//https://github.com/nuspell/nuspell/blob/45d383c0e2f25e4ea48ee8efeca53c2bb51a3510/src/tools/munch.h
+public class WordMuncher{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(WordMuncher.class);
+
+
+	private final AffixData affixData;
+	//	private final DictionaryParser dicParser;
+
+
+	public WordMuncher(final AffixData affixData, final DictionaryParser dicParser){
+		Objects.requireNonNull(affixData);
+		//		Objects.requireNonNull(dicParser);
+
+		this.affixData = affixData;
+		//		this.dicParser = dicParser;
+	}
+
+	public List<Production> inferAffixRules(final DictionaryEntry dicEntry){
+		final List<Production> originators = extractAllAffixes(dicEntry);
+		originators.size();
+
+		//TODO from the original word extract all the suffixes
+
+		//TODO from the original word extract all the prefixes
+
+		if(affixData.isComplexPrefixes()){
+			//twofold prefixes and onefold suffixes at most
+			//TODO from the original word extract all the suffixes
+			//TODO from each prefix extract all the prefixes
+
+			//TODO from the original word extract all the prefixes
+			//TODO from each suffix extract all the prefixes
+
+			//TODO from the original word extract all the prefixes
+			//TODO from each suffix extract all the suffixes
+
+			//TODO from the original word extract all the suffixes
+			//TODO from each prefix extract all the prefixes
+			//TODO from each suffix extract all the prefixes
+
+			//TODO from the original word extract all the prefixes
+			//TODO from each suffix extract all the prefixes
+			//TODO from each suffix extract all the suffixes
+
+			//TODO from the original word extract all the prefixes
+			//TODO from each suffix extract all the suffixes
+			//TODO from each suffix extract all the prefixes
+		}
+		else{
+			//twofold suffixes and onefold prefixes at most
+			//TODO from the original word extract all the prefixes
+			//TODO from each prefix extract all the suffixes
+
+			//TODO from the original word extract all the suffixes
+			//TODO from each suffix extract all the suffixes
+
+			//TODO from the original word extract all the suffixes
+			//TODO from each suffix extract all the prefixes
+
+			//TODO from the original word extract all the prefixes
+			//TODO from each prefix extract all the suffixes
+			//TODO from each suffix extract all the suffixes
+
+			//TODO from the original word extract all the suffixes
+			//TODO from each suffix extract all the suffixes
+			//TODO from each suffix extract all the prefixes
+
+			//TODO from the original word extract all the suffixes
+			//TODO from each suffix extract all the prefixes
+			//TODO from each suffix extract all the suffixes
+		}
+
+		if(LOGGER.isTraceEnabled())
+			originators.forEach(production -> LOGGER.trace("Inferred word: {}", production));
+		return originators;
+	}
+
+	List<Production> extractAllAffixes(final DictionaryEntry dicEntry){
+		final String word = dicEntry.getWord();
+		final List<String> partOfSpeech = dicEntry.getMorphologicalFieldPartOfSpeech();
+
+		final List<Production> originators = new ArrayList<>();
+		final List<RuleEntry> ruleEntries = affixData.getRuleEntries();
+		//for each rule
+		for(final RuleEntry ruleEntry : ruleEntries)
+			//for each affix entry in rule
+			for(final AffixEntry affixEntry : ruleEntry.getEntries())
+				if(affixEntry.canInverseApplyTo(word)){
+					final String originatingWord = affixEntry.undoRule(word);
+					if(originatingWord != null){
+						final Production originatingRule = Production.createFromProduction(originatingWord, affixEntry, ruleEntry.isCombinable());
+
+						//TODO undo morphological rules application
+						final AffixEntry appliedRule = originatingRule.getAppliedRule(0);
+						final List<String> originatingPartOfSpeech = getMorphologicalFieldPartOfSpeech(affixEntry.uncombineMorphologicalFields(dicEntry));
+						if(originatingPartOfSpeech == null && partOfSpeech == null
+							|| originatingPartOfSpeech != null && partOfSpeech != null && originatingPartOfSpeech.equals(partOfSpeech))
+							originators.add(originatingRule);
+					}
+				}
+
+		return originators;
+	}
+
+	public List<String> getMorphologicalFieldPartOfSpeech(final String[] morphologicalFields){
+		final String tag = MorphologicalTag.TAG_PART_OF_SPEECH.getCode();
+		final int purgeTag = tag.length();
+		return Arrays.stream(morphologicalFields != null? morphologicalFields: new String[0])
+			.filter(df -> df.startsWith(tag))
+			.map(df -> df.substring(purgeTag))
+			.collect(Collectors.toList());
+	}
+
+	public static void main(String[] args) throws IOException{
+		File affFile = FileHelper.getTemporaryUTF8File("xxx", ".aff",
+			"SET UTF-8",
+			"SFX a Y 1",
+			"SFX a 0 a .",
+			"SFX b Y 1",
+			"SFX b 0 b ."
+		);
+		String line = "a\tpo:noun";
+		AffixParser affParser = new AffixParser();
+		affParser.parse(affFile);
+		AffixData affixData = affParser.getAffixData();
+		WordMuncher muncher = new WordMuncher(affixData, null);
+		final DictionaryEntry dicEntry = DictionaryEntry.createFromDictionaryLine(line, affixData);
+		muncher.extractAllAffixes(dicEntry);
+	}
+
+	private List<Production> extractAllAffixes(final String word, final String partOfSpeech){
+		final List<Production> originatingRules = new ArrayList<>();
+		final DictionaryEntry nullDicEntry = DictionaryEntry.createFromDictionaryLine(word, affixData);
+		final List<RuleEntry> ruleEntries = affixData.getRuleEntries();
+		for(final RuleEntry ruleEntry : ruleEntries){
+			final List<Production> originatingRulesFromEntry = new ArrayList<>();
+			for(final AffixEntry affixEntry : ruleEntry.getEntries())
+				if(!affixEntry.hasContinuationFlags() && affixEntry.canInverseApplyTo(word)){
+					final String originatingWord = affixEntry.undoRule(word);
+					if(originatingWord != null){
+						final Production originatingRule = Production.createFromProduction(originatingWord, affixEntry, ruleEntry.isCombinable());
+						if(partOfSpeech == null || !originatingRule.hasPartOfSpeech() || originatingRule.hasPartOfSpeech(partOfSpeech))
+							originatingRulesFromEntry.add(originatingRule);
+					}
+				}
+			if(!originatingRulesFromEntry.isEmpty()){
+				//originatingRulesFromEntry should not have productions from identical word
+				final Map<String, List<Production>> wordBucket = SetHelper.bucket(originatingRulesFromEntry, rule -> rule.getWord());
+				final boolean identicalOriginatingWord = wordBucket.values().stream().anyMatch(prods -> prods.size() > 1);
+				if(!identicalOriginatingWord)
+					originatingRules.addAll(originatingRulesFromEntry);
+			}
+		}
+		return originatingRules;
+	}
+
+}
