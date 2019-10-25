2a016a8fe586fca25add074c27a7f90b691712df
Mauro Trevisan <mauro.simioni@targaubiest.com>
2019-10-25 15:03:58 +0200

    no message

--- src/main/java/unit731/hunspeller/services/XMLParser.java
+++ src/main/java/unit731/hunspeller/services/XMLParser.java
@@ -12,6 +12,7 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -52,9 +53,9 @@ public class XMLParser{
 		return DOCUMENT_BUILDER.newDocument();
 	}
 
+	/** Transform the DOM Object to an XML File */
 	public static void createXML(final File xmlFile, final Document doc, final Pair<String, String>... properties)
 			throws TransformerException{
-		//transform the DOM Object to an XML File
 		final TransformerFactory transformerFactory = TransformerFactory.newInstance();
 		final Transformer transformer = transformerFactory.newTransformer();
 		if(properties != null)
@@ -62,6 +63,8 @@ public class XMLParser{
 				transformer.setOutputProperty(property.getKey(), property.getValue());
 		final DOMSource domSource = new DOMSource(doc);
 		final StreamResult streamResult = new StreamResult(xmlFile);
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "3");
 		transformer.transform(domSource, streamResult);
 	}
 
