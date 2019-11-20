f823711b21128fdfbed443e1f46a81a3ee954606
Mauro Trevisan <mauro.simioni@targaubiest.com>
2019-11-20 16:49:37 +0100

    no message

---
+++ src/main/java/unit731/hunspeller/gui/JTagLabel.java
@@ -0,0 +1,13 @@
+package unit731.hunspeller.gui;
+
+import javax.swing.*;
+
+
+public class JTagLabel extends JLabel{
+
+	//TODO
+	//format GUI to look like `xgajho`
+	//on enter in a jtextfield, create and place the tag, remove the text just inserted
+	//on select and canc, remove the tag
+
+}
