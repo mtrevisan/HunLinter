4188526dbb03d86791403d570b3d0a6e920f6e07
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-01-21 12:10:14 +0100

    no message

--- src/main/java/unit731/hunlinter/services/TimeWatch.java
+++ src/main/java/unit731/hunlinter/services/TimeWatch.java
@@ -8,6 +8,10 @@ import org.apache.commons.lang3.StringUtils;
 
 public class TimeWatch{
 
+	private static final String TIMER_NOT_STOPPED = "timer not stopped";
+	private static final String MINUTES = "min";
+	private static final String SECONDS = "s";
+
 	private long start;
 	private long end;
 
@@ -35,30 +39,42 @@ public class TimeWatch{
 		return (end > 0l? end - start: 0l);
 	}
 
-	public long time(TimeUnit unit){
+	public long time(final TimeUnit unit){
 		return unit.convert(time(), TimeUnit.NANOSECONDS);
 	}
 
 	public String toStringMinuteSeconds(){
-		final StringJoiner sj = new StringJoiner(StringUtils.SPACE);
-		final long mins = time(TimeUnit.MINUTES);
-		if(mins > 0)
-			sj.add(Long.toString(mins)).add("min");
-		final long secs = time(TimeUnit.SECONDS) - mins * 60l;
-		if(mins == 0 || secs > 0)
-			sj.add(Long.toString(secs)).add("s");
-		return sj.toString();
+		if(end > 0l){
+			final long delta = time(TimeUnit.SECONDS);
+
+			final StringJoiner sj = new StringJoiner(StringUtils.SPACE);
+			final long mins = delta / 60l;
+			if(mins > 0)
+				sj.add(Long.toString(mins)).add(MINUTES);
+			final long secs = delta - mins * 60l;
+			if(mins == 0 || secs > 0)
+				sj.add(Long.toString(secs)).add(SECONDS);
+			return sj.toString();
+		}
+		else
+			return TIMER_NOT_STOPPED;
 	}
 
 	public String toStringMinuteSecondsMillis(){
-		final StringJoiner sj = new StringJoiner(StringUtils.SPACE);
-		final long mins = time(TimeUnit.MINUTES);
-		if(mins > 0)
-			sj.add(Long.toString(mins)).add("min");
-		final long millis = time(TimeUnit.MILLISECONDS) - mins * 60_000l;
-		if(mins == 0 || millis > 0)
-			sj.add(String.format(Locale.ROOT, "%.3f s", millis / 1000.));
-		return sj.toString();
+		if(end > 0l){
+			final long delta = time(TimeUnit.MILLISECONDS);
+
+			final StringJoiner sj = new StringJoiner(StringUtils.SPACE);
+			final long mins = delta / 60_000l;
+			if(mins > 0)
+				sj.add(Long.toString(mins)).add(MINUTES);
+			final long millis = delta - mins * 60_000l;
+			if(mins == 0 || millis > 0)
+				sj.add(String.format(Locale.ROOT, "%.3f", millis / 1000.)).add(SECONDS);
+			return sj.toString();
+		}
+		else
+			return TIMER_NOT_STOPPED;
 	}
 
 }
