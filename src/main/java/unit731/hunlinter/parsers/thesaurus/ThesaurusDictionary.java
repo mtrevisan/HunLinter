2d46fd9cd10bbbd92ed615c7d6f83223fc06439e
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-01-22 12:18:07 +0100

    no message

--- src/main/java/unit731/hunlinter/parsers/thesaurus/ThesaurusDictionary.java
+++ src/main/java/unit731/hunlinter/parsers/thesaurus/ThesaurusDictionary.java
@@ -2,7 +2,6 @@ package unit731.hunlinter.parsers.thesaurus;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
