dbc5be935f46b924d4ee0a797f4a012c56f9ae02
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-02-12 16:29:00 +0100

    no message

--- src/main/java/unit731/hunlinter/parsers/workers/ThesaurusCorrectnessWorker.java
+++ src/main/java/unit731/hunlinter/parsers/workers/ThesaurusCorrectnessWorker.java
@@ -1,35 +1,69 @@
 package unit731.hunlinter.parsers.workers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import unit731.hunlinter.Backbone;
+import unit731.hunlinter.parsers.thesaurus.ThesaurusEntry;
 import unit731.hunlinter.parsers.thesaurus.ThesaurusParser;
+import unit731.hunlinter.parsers.workers.core.WorkerBase;
 import unit731.hunlinter.parsers.workers.core.WorkerData;
-import unit731.hunlinter.parsers.workers.core.WorkerDictionaryBase;
 
 import java.text.MessageFormat;
+import java.util.List;
 import java.util.Objects;
-import java.util.function.BiConsumer;
 
 
-public class ThesaurusCorrectnessWorker extends WorkerDictionaryBase{
+public class ThesaurusCorrectnessWorker extends WorkerBase<Void, Void>{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ThesaurusCorrectnessWorker.class);
 
 	public static final String WORKER_NAME = "Thesaurus correctness checking";
 
 	private static final MessageFormat WORD_IS_NOT_SYLLABABLE = new MessageFormat("Word {0} ({1}) is not syllabable");
 
 
+	private final ThesaurusParser theParser;
+
+
 	public ThesaurusCorrectnessWorker(final ThesaurusParser theParser){
 		Objects.requireNonNull(theParser);
 
-		//TODO pass theParser.getSynonymsDictionary() and check for correctness
-		final BiConsumer<String, Integer> lineProcessor = (line, row) -> {
-			//TODO
-		};
-		final WorkerData data = WorkerData.createParallelPreventExceptionRelaunch(WORKER_NAME, dicParser);
-		createReadWorker(data, lineProcessor);
+		this.theParser = theParser;
+
+		workerData = WorkerData.createParallelPreventExceptionRelaunch(WORKER_NAME);
 	}
 
 	@Override
-	public String getWorkerName(){
-		return WORKER_NAME;
+	protected Void doInBackground(){
+		try{
+			exception = null;
+
+			LOGGER.info(Backbone.MARKER_APPLICATION, WORKER_NAME);
+			setProgress(0);
+
+			final List<ThesaurusEntry> dictionary = theParser.getSynonymsDictionary();
+			int i = 0;
+			final int size = dictionary.size();
+			for(final ThesaurusEntry entry : dictionary){
+				//TODO check if each part of `entry`, with appropriate PoS, exists
+
+				setProgress(i * 100 / size);
+			}
+
+			LOGGER.info(Backbone.MARKER_APPLICATION, "File sorted");
+
+			setProgress(100);
+		}
+		catch(final Exception e){
+			exception = e;
+
+			LOGGER.error(Backbone.MARKER_APPLICATION, e.getMessage());
+			LOGGER.info(Backbone.MARKER_APPLICATION, "Stopped reading Thesaurus file");
+
+			cancel(true);
+		}
+
+		return null;
 	}
 
 }
