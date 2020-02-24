ee614f6b12286090295b0dc4840b49822fc7c200
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-02-24 18:01:24 +0100

    no message

--- src/main/java/unit731/hunlinter/workers/WorkerManager.java
+++ src/main/java/unit731/hunlinter/workers/WorkerManager.java
@@ -1,5 +1,10 @@
 package unit731.hunlinter.workers;
 
+import unit731.hunlinter.gui.GUIUtils;
+import unit731.hunlinter.workers.core.WorkerAbstract;
+
+import javax.swing.*;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +16,16 @@ public class WorkerManager{
 
 	public void addWorker(final String workerName, final Runnable onEnding){
 		WORKER_ON_ENDING.put(workerName, onEnding);
+	}
+
+	public void checkAbortion(final WorkerAbstract<?, ?> worker, final Component parentComponent){
+		if(worker != null && worker.getState() == SwingWorker.StateValue.STARTED){
+			final String workerName = worker.getWorkerData()
+				.getWorkerName();
+			final Runnable onEnding = WORKER_ON_ENDING.get(workerName);
+//			final Runnable resumeTask = () -> setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+			GUIUtils.askUserToAbort(worker, parentComponent, onEnding, null);
+		}
 	}
 
 	public void onEnd(final String workerName){
