01a51dbc5ea45ded4cc032974e3411b49d50e7b9
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-03-10 09:31:43 +0100

    no message

--- src/test/java/unit731/hunlinter/services/fsa/builders/FSABuilderTest.java
+++ src/test/java/unit731/hunlinter/services/fsa/builders/FSABuilderTest.java
@@ -1,6 +1,5 @@
 package unit731.hunlinter.services.fsa.builders;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import unit731.hunlinter.services.fsa.FSA;
@@ -11,6 +10,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 
@@ -80,18 +80,40 @@ class FSABuilderTest{
 
 	@Test
 	void testRandom25000_largerAlphabet(){
-		List<String> input = new ArrayList<>(25_000);
-		for(int i = 0; i < 25_000; i ++)
-			input.add(RandomStringUtils.randomAlphanumeric(1, 20));
-		input.sort(Comparator.naturalOrder());
+		List<byte[]> in = generateRandom(25_000, 1, 20, 0, 255);
 
-		List<byte[]> in = input.stream()
-			.map(word -> word.getBytes(StandardCharsets.UTF_8))
-			.collect(Collectors.toList());
 		FSA fsa = FSABuilder.build(in);
 
 		FSATestUtils.checkCorrect(in, fsa);
 		FSATestUtils.checkMinimal(fsa);
+	}
+
+	@Test
+	public void testRandom25000_smallAlphabet(){
+		List<byte[]> in = generateRandom(40, 1, 20, 0, 3);
+
+		FSA fsa = FSABuilder.build(in);
+
+		FSATestUtils.checkCorrect(in, fsa);
+		FSATestUtils.checkMinimal(fsa);
+	}
+
+	/** Generate a sorted list of random sequences */
+	private List<byte[]> generateRandom(int count, int lengthMin, int lengthMax, int alphabetMin, int alphabetMax){
+		final List<byte[]> input = new ArrayList<>();
+		final Random rnd = new Random(System.currentTimeMillis());
+		for(int i = 0; i < count; i ++)
+			input.add(randomByteSequence(rnd, lengthMin, lengthMax, alphabetMin, alphabetMax));
+		Collections.sort(input, FSABuilder.LEXICAL_ORDERING);
+		return input;
+	}
+
+	/** Generate a random string */
+	private byte[] randomByteSequence(Random rnd, int lengthMin, int lengthMax, int alphabetMin, int alphabetMax){
+		byte[] bytes = new byte[lengthMin + rnd.nextInt(lengthMax - lengthMin + 1)];
+		for(int i = 0; i < bytes.length; i ++)
+			bytes[i] = (byte) (alphabetMin + rnd.nextInt(alphabetMax - alphabetMin + 1));
+		return bytes;
 	}
 
 }
