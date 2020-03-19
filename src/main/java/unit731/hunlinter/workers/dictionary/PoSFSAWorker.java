424b0a2c558ce4e5bf198e792955c1e1571b61e1
Mauro Trevisan <851903+mtrevisan@users.noreply.github.com>
2020-03-19 10:41:18 +0100

    no message

--- src/main/java/unit731/hunlinter/workers/dictionary/PoSFSAWorker.java
+++ src/main/java/unit731/hunlinter/workers/dictionary/PoSFSAWorker.java
@@ -36,12 +36,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CodingErrorAction;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -146,41 +143,6 @@ public class PoSFSAWorker extends WorkerDictionary{
 			processLines(dicPath, charset, lineProcessor);
 			closeWriter(writer);
 
-//			LOGGER.info(ParserManager.MARKER_APPLICATION, "Post-processing");
-//
-//			final Set<String> words2 = new HashSet<>();
-//			for(Pair<Integer, String> l : lines){
-//				final DictionaryEntry dicEntry = wordGenerator.createFromDictionaryLine(l.getValue());
-//				final List<Production> productions = wordGenerator.applyAffixRules(dicEntry);
-//
-//				productions.stream()
-//					.map(Production::toStringPoSFSA)
-//					.flatMap(List::stream)
-//					.forEach(words2::add);
-//			}
-//
-//
-//			try{
-//				final String filenameNoExtension = FilenameUtils.removeExtension(outputFile.getAbsolutePath());
-//				final File outputInfoFile = new File(filenameNoExtension + ".info");
-//				if(!outputInfoFile.exists()){
-//					final List<String> content = Arrays.asList(
-//						"fsa.dict.separator=" + Production.POS_FSA_SEPARATOR,
-//						"fsa.dict.encoding=" + charset.name().toLowerCase(),
-//						"fsa.dict.encoder=prefix");
-//					FileHelper.saveFile(outputInfoFile.toPath(), StringUtils.CR, charset, content);
-//				}
-//
-//				buildFSA(new ArrayList<>(words2), outputFile.toString(), filenameNoExtension + ".dict");
-//
-//				finalizeProcessing("File written: " + filenameNoExtension + ".dict");
-//
-//				FileHelper.browse(outputFile);
-//			}
-//			catch(final Exception e){
-//				LOGGER.warn("Exception while creating the FSA file for Part–of–Speech", e);
-//			}
-
 			return supportFile;
 		};
 		final Function<File, File> step2 = file -> {
@@ -230,6 +192,8 @@ public class PoSFSAWorker extends WorkerDictionary{
 
 			processLines(file.toPath(), charset, fsaProcessor);
 
+			file.delete();
+
 			return builder.complete();
 		};
 		final Function<FSA, File> step4 = fsa -> {
@@ -239,6 +203,13 @@ public class PoSFSAWorker extends WorkerDictionary{
 			final CFSA2Serializer serializer = new CFSA2Serializer();
 			try(final OutputStream os = new BufferedOutputStream(Files.newOutputStream(outputFile.toPath()))){
 				serializer.serialize(fsa, os);
+
+				//validation: try to scan the input
+				final DictionaryLookup dictionaryLookup = new DictionaryLookup(new Dictionary(fsa, metadata));
+				//noinspection StatementWithEmptyBody
+				for(final Iterator<?> i = dictionaryLookup.iterator(); i.hasNext(); i.next()){
+					//do nothing, just scan and make sure no exceptions are thrown.
+				}
 
 				return outputFile;
 			}
@@ -303,79 +274,6 @@ public class PoSFSAWorker extends WorkerDictionary{
 		//lexical order
 		Collections.sort(words);
 		return words;
-	}
-
-	private void buildFSA(List<String> words, final String input, final String output) throws Exception{
-		final Path inputPath = Path.of(input);
-		final Path outputPath = Path.of(output);
-
-		final Path metadataPath = DictionaryMetadata.getExpectedMetadataLocation(inputPath);
-
-		if(!Files.isRegularFile(metadataPath))
-			throw new IllegalArgumentException("Dictionary metadata file for the input does not exist: " + metadataPath
-				+ "\r\nThe metadata file (with at least the column separator and byte encoding) is required. Check out the examples.");
-
-//		final Path outputPath = metadataPath.resolveSibling(metadataPath.getFileName().toString().replaceAll("\\." + DictionaryMetadata.METADATA_FILE_EXTENSION + "$", ".dict"));
-
-		final DictionaryMetadata metadata;
-		try(final InputStream is = new BufferedInputStream(Files.newInputStream(metadataPath))){
-			metadata = DictionaryMetadata.read(is);
-		}
-
-		final CharsetDecoder charsetDecoder = metadata.getDecoder()
-			.onMalformedInput(CodingErrorAction.REPORT)
-			.onUnmappableCharacter(CodingErrorAction.REPORT);
-
-		final byte separator = metadata.getSeparator();
-		final ISequenceEncoder sequenceEncoder = metadata.getSequenceEncoderType().get();
-
-		if(!words.isEmpty()){
-			//lexical order
-			Collections.sort(words);
-
-			Iterator<String> i = words.iterator();
-			byte[] row = StringHelper.getRawBytes(i.next());
-			final int separatorCount = countOf(separator, row);
-
-			if(separatorCount < 1 || separatorCount > 2){
-				String separatorCharacter = (Character.isJavaIdentifierPart(metadata.getSeparatorAsChar())? "'" + metadata.getSeparatorAsChar() + "'": "0x" + Integer.toHexString((int) separator & 0xff));
-				throw new IllegalArgumentException("Invalid input. Each row must consist of [base,inflected,tag?] columns, where ',' is a separator character"
-					+ " (declared as: " + separatorCharacter + "). This row contains " + separatorCount + " separator characters: " + new String(row, charsetDecoder.charset()));
-			}
-
-			while(i.hasNext()){
-				row = StringHelper.getRawBytes(i.next());
-				final int count = countOf(separator, row);
-				if(count != separatorCount)
-					throw new IllegalArgumentException("The number of separators (" + count + ") is inconsistent with previous lines: " + new String(row, charsetDecoder.charset()));
-			}
-		}
-
-		words = encode(words, separator, sequenceEncoder);
-		final List<byte[]> in = new ArrayList<>();
-		LoopHelper.forEach(words, word -> in.add(StringHelper.getRawBytes(word)));
-		final FSABuilder builder = new FSABuilder();
-		final FSA fsa = builder.build(in);
-
-		final CFSA2Serializer serializer = new CFSA2Serializer();
-		try(final OutputStream os = new BufferedOutputStream(Files.newOutputStream(outputPath))){
-			serializer.serialize(fsa, os);
-		}
-
-		//if validating, try to scan the input
-		final DictionaryLookup dictionaryLookup = new DictionaryLookup(new Dictionary(fsa, metadata));
-		//noinspection StatementWithEmptyBody
-		for(final Iterator<?> i = dictionaryLookup.iterator(); i.hasNext(); i.next()){
-			//do nothing, just scan and make sure no exceptions are thrown.
-		}
-	}
-
-	private static int countOf(final byte separator, final byte[] row){
-		int cnt = 0;
-		for(int i = row.length; --i >= 0; )
-			if(row[i] == separator)
-				cnt ++;
-		return cnt;
 	}
 
 	private static int indexOf(final byte separator, final byte[] row, int fromIndex){
