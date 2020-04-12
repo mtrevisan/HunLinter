15bd863dc2896aa7e6122eaa8a9e91e7f73dfdbe
Mauro Trevisan <851903+mtrevisan@users.noreply.github.com>
2020-04-12 17:45:32 +0200

    no message

--- src/main/java/unit731/hunlinter/parsers/vos/Inflection.java
+++ src/main/java/unit731/hunlinter/parsers/vos/Inflection.java
@@ -9,7 +9,6 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import unit731.hunlinter.parsers.affix.strategies.FlagParsingStrategy;
 import unit731.hunlinter.parsers.enums.AffixType;
-import unit731.hunlinter.services.datastructures.FixedArray;
 
 import static unit731.hunlinter.services.system.LoopHelper.forEach;
 
@@ -43,12 +42,11 @@ public class Inflection extends DictionaryEntry{
 
 	public static Inflection createFromInflection(final String word, final AffixEntry appliedEntry,
 			final DictionaryEntry dicEntry, final String[] remainingContinuationFlags, final boolean combinable){
-		final FixedArray<String> continuationFlags = appliedEntry.combineContinuationFlags(remainingContinuationFlags);
+		final String[] continuationFlags = appliedEntry.combineContinuationFlags(remainingContinuationFlags);
 		final String[] morphologicalFields = appliedEntry.combineMorphologicalFields(dicEntry);
 		final AffixEntry[] appliedRules = new AffixEntry[]{appliedEntry};
 		final DictionaryEntry[] compoundEntries = extractCompoundEntries(dicEntry);
-		return new Inflection(word, continuationFlags.extractCopyOrNull(), morphologicalFields, combinable,
-			appliedRules, compoundEntries);
+		return new Inflection(word, continuationFlags, morphologicalFields, combinable, appliedRules, compoundEntries);
 	}
 
 	public static Inflection clone(final DictionaryEntry dicEntry){
