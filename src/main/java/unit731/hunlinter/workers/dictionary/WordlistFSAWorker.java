05be3bb5be7c50829f3459ca29f77e1977ca1391
Mauro Trevisan <851903+mtrevisan@users.noreply.github.com>
2020-04-13 18:36:22 +0200

    no message

--- src/main/java/unit731/hunlinter/workers/dictionary/WordlistFSAWorker.java
+++ src/main/java/unit731/hunlinter/workers/dictionary/WordlistFSAWorker.java
@@ -8,7 +8,9 @@ import unit731.hunlinter.parsers.dictionary.DictionaryParser;
 import unit731.hunlinter.parsers.dictionary.generators.WordGenerator;
 import unit731.hunlinter.parsers.vos.DictionaryEntry;
 import unit731.hunlinter.parsers.vos.Inflection;
+import unit731.hunlinter.services.datastructures.SimpleDynamicArray;
 import unit731.hunlinter.services.fsa.FSA;
+import unit731.hunlinter.services.fsa.builders.LexicographicalComparator;
 import unit731.hunlinter.services.fsa.serializers.CFSA2Serializer;
 import unit731.hunlinter.services.fsa.builders.FSABuilder;
 import unit731.hunlinter.services.fsa.stemming.DictionaryMetadata;
@@ -64,42 +66,44 @@ public class WordlistFSAWorker extends WorkerDictionary{
 //		final SequenceEncoderInterface sequenceEncoder = metadata.getSequenceEncoderType().get();
 
 
-		final Set<String> words = new HashSet<>();
+		final SimpleDynamicArray<byte[]> encodings = new SimpleDynamicArray<>(byte[].class, 50_000_000, 1.2f);
 		final Consumer<IndexDataPair<String>> lineProcessor = indexData -> {
 			final String line = indexData.getData();
 			final DictionaryEntry dicEntry = wordGenerator.createFromDictionaryLine(line);
 			final Inflection[] inflections = wordGenerator.applyAffixRules(dicEntry);
 
-			forEach(inflections, prod -> words.add(prod.toString()));
+			final byte[][] words = new byte[inflections.length][];
+			for(int i = 0; i < inflections.length; i ++)
+				words[i] = StringHelper.getRawBytes(inflections[i].getWord());
+			encodings.addAll(words);
 
 			sleepOnPause();
 		};
 		final FSABuilder builder = new FSABuilder();
 		final Consumer<byte[]> fsaProcessor = builder::add;
 
-		final Function<Void, Set<String>> step1 = ignored -> {
+		final Function<Void, SimpleDynamicArray<byte[]>> step1 = ignored -> {
 			prepareProcessing("Reading dictionary file (step 1/4)");
 
 			final Path dicPath = dicParser.getDicFile().toPath();
 			processLines(dicPath, charset, lineProcessor);
 
-			return words;
+			return encodings;
 		};
-		final Function<Set<String>, String[]> step2 = set -> {
+		final Function<SimpleDynamicArray<byte[]>, SimpleDynamicArray<byte[]>> step2 = list -> {
 			resetProcessing("Sorting (step 2/4)");
 
 			//sort list
-			final String[] uniqueWords = set.toArray(String[]::new);
-			SmoothSort.sort(uniqueWords, Comparator.naturalOrder(),
+			SmoothSort.sort(list.data, 0, list.limit, LexicographicalComparator.lexicographicalComparator(),
 				percent -> {
 					setProgress(percent, 100);
 
 					sleepOnPause();
 				});
 
-			return uniqueWords;
+			return list;
 		};
-		final Function<String[], FSA> step3 = list -> {
+		final Function<SimpleDynamicArray<byte[]>, FSA> step3 = list -> {
 			resetProcessing("Creating FSA (step 3/4)");
 
 			getWorkerData()
@@ -108,19 +112,22 @@ public class WordlistFSAWorker extends WorkerDictionary{
 
 			int progress = 0;
 			int progressIndex = 0;
-			final int progressStep = (int)Math.ceil(list.length / 100.f);
-			for(int index = 0; index < list.length; index ++){
-				final byte[] encoding = StringHelper.getRawBytes(list[index]);
+			final int progressStep = (int)Math.ceil(list.limit / 100.f);
+			for(int index = 0; index < list.limit; index ++){
+				final byte[] encoding = list.data[index];
 				fsaProcessor.accept(encoding);
 
 				//release memory
-				list[index] = null;
+				list.data[index] = null;
 
 				if(++ progress % progressStep == 0)
 					setProgress(++ progressIndex, 100);
 
 				sleepOnPause();
 			}
+
+			//release memory
+			list.clear();
 
 			return builder.complete();
 		};
