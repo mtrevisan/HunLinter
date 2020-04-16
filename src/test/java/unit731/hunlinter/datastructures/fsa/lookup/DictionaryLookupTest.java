62fe974aa82f6745146e897cf687e3d7e6b96cfc
Mauro Trevisan <851903+mtrevisan@users.noreply.github.com>
2020-04-16 17:32:02 +0200

    no message

--- src/test/java/unit731/hunlinter/datastructures/fsa/lookup/DictionaryLookupTest.java
+++ src/test/java/unit731/hunlinter/datastructures/fsa/lookup/DictionaryLookupTest.java
@@ -49,37 +49,40 @@ class DictionaryLookupTest{
 	@Test
 	void prefixDictionaries() throws IOException{
 		final URL url = getClass().getResource("/services/fsa/lookup/prefix.dict");
-		final DictionaryLookup s = new DictionaryLookup(Dictionary.read(url));
+		Dictionary d = Dictionary.read(url);
+		final DictionaryLookup s = new DictionaryLookup(d);
 
-		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzeczypospolitej"));
-		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzecząpospolitą"));
+		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzeczypospolitej", d));
+		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzecząpospolitą", d));
 
 		//this word is not in the dictionary
-		assertNoStemFor(s, "martygalski");
+		assertNoStemFor(s, "martygalski", d);
 	}
 
 	@Test
 	void inputConversion() throws IOException{
 		final URL url = getClass().getResource("/services/fsa/lookup/prefix.dict");
-		final DictionaryLookup s = new DictionaryLookup(Dictionary.read(url));
+		Dictionary d = Dictionary.read(url);
+		final DictionaryLookup s = new DictionaryLookup(d);
 
-		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzecz\\apospolit\\a"));
+		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzecz\\apospolit\\a", d));
 
-		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "krowa\\apospolit\\a"));
+		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "krowa\\apospolit\\a", d));
 	}
 
 	@Test
 	void infixDictionaries() throws IOException{
 		final URL url = getClass().getResource("/services/fsa/lookup/infix.dict");
-		final DictionaryLookup s = new DictionaryLookup(Dictionary.read(url));
+		Dictionary d = Dictionary.read(url);
+		final DictionaryLookup s = new DictionaryLookup(d);
 
-		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzeczypospolitej"));
-		Assertions.assertArrayEquals(new String[]{"Rzeczycki", "adj:pl:nom:m"}, stem(s, "Rzeczyccy"));
-		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzecząpospolitą"));
+		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzeczypospolitej", d));
+		Assertions.assertArrayEquals(new String[]{"Rzeczycki", "adj:pl:nom:m"}, stem(s, "Rzeczyccy", d));
+		Assertions.assertArrayEquals(new String[]{"Rzeczpospolita", "subst:irreg"}, stem(s, "Rzecząpospolitą", d));
 		//this word is not in the dictionary
-		assertNoStemFor(s, "martygalski");
+		assertNoStemFor(s, "martygalski", d);
 		//this word uses characters that are outside of the encoding range of the dictionary
-		assertNoStemFor(s, "Rzeczyckiõh");
+		assertNoStemFor(s, "Rzeczyckiõh", d);
 	}
 
 //	@Test
@@ -134,10 +137,11 @@ class DictionaryLookupTest{
 	@Test
 	void multibyteEncodingUTF8() throws IOException{
 		final URL url = getClass().getResource("/services/fsa/lookup/diacritics-utf8.dict");
-		final DictionaryLookup s = new DictionaryLookup(Dictionary.read(url));
+		Dictionary d = Dictionary.read(url);
+		final DictionaryLookup s = new DictionaryLookup(d);
 
-		Assertions.assertArrayEquals(new String[]{"merge", "001"}, stem(s, "mergeam"));
-		Assertions.assertArrayEquals(new String[]{"merge", "002"}, stem(s, "merseserăm"));
+		Assertions.assertArrayEquals(new String[]{"merge", "001"}, stem(s, "mergeam", d));
+		Assertions.assertArrayEquals(new String[]{"merge", "002"}, stem(s, "merseserăm", d));
 	}
 
 //	@Test
@@ -155,12 +159,13 @@ class DictionaryLookupTest{
 //	@Test
 //	void inputWithSeparators() throws IOException{
 //		final URL url = getClass().getResource("/services/fsa/lookup/separators.dict");
-//		final DictionaryLookup s = new DictionaryLookup(Dictionary.read(url));
+//		Dictionary d = Dictionary.read(url);
+//		final DictionaryLookup s = new DictionaryLookup(d);
 //
 //		//attemp to reconstruct input sequences using WordData iterator
 //		List<String> sequences = new ArrayList<>();
 //		for(WordData wd : s)
-//			sequences.add(wd.getWord() + " " + toUTF8String(wd.getStem()) + " " + toUTF8String(wd.getTag()));
+//			sequences.add(wd.getWord() + " " + toString(wd.getStem(), d) + " " + toString(wd.getTag(), d));
 //		Collections.sort(sequences);
 //
 //		Assertions.assertEquals("token1 null null", sequences.get(0));
@@ -190,21 +195,21 @@ class DictionaryLookupTest{
 	}
 
 
-	private static void assertNoStemFor(DictionaryLookup s, String word){
-		Assertions.assertArrayEquals(new String[]{}, stem(s, word));
+	private static void assertNoStemFor(DictionaryLookup s, String word, Dictionary d){
+		Assertions.assertArrayEquals(new String[]{}, stem(s, word, d));
 	}
 
-	private static String[] stem(DictionaryLookup s, String word){
+	private static String[] stem(DictionaryLookup s, String word, Dictionary d){
 		List<String> result = new ArrayList<>();
 		for(WordData wd : s.lookup(word)){
-			result.add(toUTF8String(wd.getStem()));
-			result.add(toUTF8String(wd.getTag()));
+			result.add(toString(wd.getStem(), d));
+			result.add(toString(wd.getTag(), d));
 		}
 		return result.toArray(String[]::new);
 	}
 
-	private static String toUTF8String(byte[] array){
-		return (array != null? new String(array, StandardCharsets.UTF_8): null);
+	private static String toString(byte[] array, Dictionary d){
+		return (array != null? new String(array,d.metadata.getCharset()): null);
 	}
 
 }
