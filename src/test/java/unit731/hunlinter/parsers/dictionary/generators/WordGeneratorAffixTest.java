2b959997bbbce028bedd20b0deff36f52cffda10
Mauro Trevisan <851903+mtrevisan@users.noreply.github.com>
2020-05-02 11:19:18 +0200

    no message

--- src/test/java/unit731/hunlinter/parsers/dictionary/generators/WordGeneratorAffixTest.java
+++ src/test/java/unit731/hunlinter/parsers/dictionary/generators/WordGeneratorAffixTest.java
@@ -431,7 +431,7 @@ class WordGeneratorAffixTest extends TestBase{
 			DictionaryEntry dicEntry = wordGenerator.createFromDictionaryLine(line);
 			wordGenerator.applyAffixRules(dicEntry);
 		});
-		Assertions.assertEquals("Twofold rule violated for 'p1aas1/S2,P2	st:aa	from	S1 > P1 from S1 > P1' (S1 > P1 still has rules P2)", exception.getMessage());
+		Assertions.assertEquals("Twofold rule violated for 'p1aas1/S2,P2\tst:aa\tfrom\tSFX S1 0 s1/S2P1 . > PFX P1 0 p1/P2 . from S1 > P1' (S1 > P1 still has rules P2)", exception.getMessage());
 	}
 
 	@Test
@@ -462,7 +462,7 @@ class WordGeneratorAffixTest extends TestBase{
 			DictionaryEntry dicEntry = wordGenerator.createFromDictionaryLine(line);
 			wordGenerator.applyAffixRules(dicEntry);
 		});
-		Assertions.assertEquals("Twofold rule violated for 'ga/A,B,C,D,E	st:a	from	G from G' (G still has rules E)", exception.getMessage());
+		Assertions.assertEquals("Twofold rule violated for 'ga/A,B,C,D,E\tst:a\tfrom\tPFX G 0 g/E . from G' (G still has rules E)", exception.getMessage());
 	}
 
 
@@ -590,7 +590,7 @@ class WordGeneratorAffixTest extends TestBase{
 			DictionaryEntry dicEntry = wordGenerator.createFromDictionaryLine(line);
 			wordGenerator.applyAffixRules(dicEntry);
 		});
-		Assertions.assertEquals("Twofold rule violated for 'ag/A,B,C,D,E	st:a	from	G from G' (G still has rules E)", exception.getMessage());
+		Assertions.assertEquals("Twofold rule violated for 'ag/A,B,C,D,E\tst:a\tfrom\tSFX G 0 g/E . from G' (G still has rules E)", exception.getMessage());
 	}
 
 
