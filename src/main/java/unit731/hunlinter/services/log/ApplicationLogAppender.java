2e8ad15459ae99025caa6d45074e8999cd52130e
Mauro Trevisan <mauro.simioni@targaubiest.com>
2020-09-18 15:55:41 +0200

    added settings to disable warning errors logs

--- src/main/java/unit731/hunlinter/services/log/ApplicationLogAppender.java
+++ src/main/java/unit731/hunlinter/services/log/ApplicationLogAppender.java
@@ -81,8 +81,8 @@ public class ApplicationLogAppender extends AppenderBase<ILoggingEvent>{
 			final byte[] encoded = encoder.encode(eventObject);
 			final String message = new String(encoded, StandardCharsets.UTF_8);
 
-			//FIXME
-			if(!preferences.getBoolean(ReportWarningsAction.REPORT_WARNINGS, true) && message.contains("WARN:"))
+			//FIXME?
+			if(!preferences.getBoolean(ReportWarningsAction.REPORT_WARNINGS, true) && message.contains(" WARN: "))
 				return;
 
 			final Marker marker = eventObject.getMarker();
