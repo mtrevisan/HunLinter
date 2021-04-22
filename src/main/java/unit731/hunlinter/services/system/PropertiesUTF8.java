24b71e3f9aca732017a5e1917d9be71ad70e5b4f
Mauro Trevisan <mauro.simioni@targaubiest.com>
2021-04-22 11:17:44 +0200

    Revert "Revert "Revert "no message"""
    This reverts commit 054eab39f1a1c110674783a705293f94f488a643.

--- src/main/java/unit731/hunlinter/services/system/PropertiesUTF8.java
+++ src/main/java/unit731/hunlinter/services/system/PropertiesUTF8.java
@@ -81,12 +81,12 @@ public class PropertiesUTF8 extends Properties{
 				line = removeWhiteSpaces(line);
 				if(!line.isEmpty() && COMMENT.indexOf(line.charAt(0)) < 0){
 					//removes the beginning separators
-					final StringBuilder property = new StringBuilder(line);
+					String property = line;
 					//reads the whole property if it is on multiple lines
 					while(continueLine(line)){
-						property.setLength(property.length() - 1);
+						property = property.substring(0, property.length() - 1);
 						line = in.readLine();
-						property.append(line);
+						property += line;
 					}
 
 					if(!property.isEmpty()){
